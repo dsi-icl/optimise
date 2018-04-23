3fb6c8f334598fa4da1d42652d02d8ee4bbccb62
Florian Guitton <f.guitton@imperial.ac.uk>
2018-04-23 09:24:53 +0200

    Add website to the version control to simplify the improvement process.

---
+++ website/scripts/optimise.js
@@ -0,0 +1,31 @@
+! function ($) {
+    "use strict";
+    $(function () {
+        var b = $(window),
+            c = $(document.body);
+        c.scrollspy({
+            target: ".bs-docs-sidebar"
+        }), b.on("load", function () {
+            c.scrollspy("refresh")
+        }), $('.bs-docs-container [href="#"]').click(function (a) {
+            $.preventDefault()
+        }), setTimeout(function () {
+            var b = $(".bs-docs-sidebar");
+            b.affix({
+                offset: {
+                    top: function () {
+                        var c = b.offset().top,
+                            d = parseInt(b.children(0).css("margin-top"), 10),
+                            e = $(".bs-docs-nav").height();
+                        return this.top = c - e - d
+                    },
+                    bottom: function () {
+                        return this.bottom = $(".bs-docs-footer").outerHeight(!0)
+                    }
+                }
+            })
+        }, 100), setTimeout(function () {
+            $(".bs-top").affix()
+        }, 100);
+    })
+}(jQuery)
