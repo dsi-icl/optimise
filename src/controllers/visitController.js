68f326196df1678695a5d5940c6dcbb98f32bde5
sou-c <sou.chon@outlook.com>
2018-05-11 19:10:45 +0100

    created visitController.js

---
+++ src/controllers/visitController.js
@@ -0,0 +1,8 @@
+const {isEmptyObject} = require('../utils/basic-utils');
+const knex = require('../utils/db-connection');
+
+class VisitController {
+    static getAllVisitOfPatient(req, res){
+        
+    }
+}
