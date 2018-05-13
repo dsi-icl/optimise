627ca1f38f3ca159987675657ddc434cdd0684bb
Chon <sou.chon@icloud.com>
2018-05-13 20:16:58 +0100

    ugly fix for the validateAndFormateDate

--- src/utils/basic-utils.js
+++ src/utils/basic-utils.js
@@ -11,8 +11,10 @@ exports.validateAndFormatDate = function(dateObj){
     //example dateObj = {"date":5, "year": 2015, "month": 1-12}
     if (dateObj.date && dateObj.year && dateObj.month){
         const daysInAMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        const days = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
+        const months = [1,2,3,4,5,6,7,8,9,10,11,12];
         if ((!(dateObj.year % 4) && dateObj.year % 100) || !(dateObj.year % 400)) {daysInAMonth[1] = 29}
-        if (dateObj.date <= daysInAMonth[dateObj.month-1]) {
+        if (dateObj.date in days && dateObj.month in months && dateObj.date <= daysInAMonth[dateObj.month-1]) {
             return dateObj.date + '/' + dateObj.month + '/' + dateObj.year;
         } else {
             return false
