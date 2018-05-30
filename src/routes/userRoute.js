7657fbd4198c2b7a14bd3c6abf9bd1434feef99e
Pierre-Marie (Radiateurs) <pierre-marie.danieau@epitech.eu>
2018-05-30 10:29:48 +0100

    Small modifications in addActionToCOllection + Comments in routes

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,6 +1,6 @@
 /**
  * Route user
- * @description Redirect request from /internalapi to the proper controller call 
+ * @description Redirect request from /internalapi and /api/users to the proper controller call 
  */
 
 const express = require('express');
@@ -13,12 +13,19 @@ user.set('x-powered-by', false);
 user.use(bodyParser.json());
 user.use(bodyParser.urlencoded({ extended: true }));
 
+// Log the user in
+// real path is /internapi/userlogin
 user.route('/userlogin')
    .post(UserController.userLogin);
 
+// Log the user out
+// real path is /internapi/userlogout
 user.route('/userlogout')
    .post(UserController.userLogout);
 
+// Interacts with the user in the DB 
+// (POST : create / DELETE : delete / PUT : modify)
+// Real path is /api/users
 user.use('/', UserController._Router);
 
 module.exports = user;
