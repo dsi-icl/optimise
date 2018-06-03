834869423f5b8452b684b6937a135851397702d8
Chon <sou.chon@icloud.com>
2018-06-03 23:14:08 +0100

    adding css-in-js

---
+++ src/css/searchPatientsById.css.js
@@ -0,0 +1,12 @@
+export default {
+    patientBanner: {
+        backgroundColor: 'grey',
+        color: 'black',
+        borderRadius: 10,
+        fontFamily: 'sans-serif',
+        height: 100,
+        margin: 10,
+        width: 200,
+        paddingTop: 50,
+        textAlign: 'center'}
+}
