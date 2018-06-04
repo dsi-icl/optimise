9a72de299ac68c3527db0711a076afd4c674de15
Chon <sou.chon@icloud.com>
2018-06-04 13:03:42 +0100

    made patientProfile demograhic template

--- src/js/App.js
+++ src/js/App.js
@@ -1,15 +1,14 @@
 import React, {Component} from 'react';
 import {SearchPatientsById} from './components/searchPatientsById.jsx';
-
-
+import {Section} from './components/patientProfile.jsx';
+import css from '../css/globalDefault.css.js';
 
 
 class App extends Component {
     render() {
         return (
-        <div className="App">
-            Type whatever you want!
-            <SearchPatientsById/>
+        <div className="App" style={css.global}>
+            <Section/>
         </div>
         );
     }
