139f5270de93eee16fb2a9419e08aa8535038d63
Florian Guitton <f.guitton@imperial.ac.uk>
2018-06-08 12:23:51 +0100

    Adding redux wrapper to the test case.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './js/App';
+import store from './js/redux/store.js';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
