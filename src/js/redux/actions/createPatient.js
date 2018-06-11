eddf6a5208a314d62fe839c98d038591cf77aea3
sou-c <sou.chon@outlook.com>
2018-06-11 13:23:52 +0100

    restructured actions

--- src/js/redux/actions/createPatient.js
+++ src/js/redux/actions/createPatient.js
@@ -1,20 +1,3 @@
-export const searchPatientsByIdRequest = searchString => ({type: 'SEARCH_PATIENTS_BY_ID_REQUEST', payload: searchString});
-export const searchResultByIdSuccess = data => ({type: 'SEARCH_RESULT_BY_ID_SUCCESS', payload: data});
-export const getPatientProfileById = (searchString) => {
-    return dispatch => {
-        dispatch(searchPatientsByIdRequest(searchString));
-        return fetch(`/api/patientProfile/${searchString}`,{
-            mode: 'cors',
-            headers: {'token': 'd86d6e50ade67a3a0569ebc84d6041ea9bac36cb'}   //change later
-        })
-            .then(res => res.json(), err => console.log(err))
-            .then(json => {
-                console.log(json);
-                dispatch(searchResultByIdSuccess(json))
-            })
-    }
-}
-
 export const createPatientRequest = patientId => ({type: 'CREATE_PATIENT_REQUEST', payload: patientId});
 export const createPatientSuccess = patientId => ({type: 'CREATE_PATIENT_SUCCESS', payload: patientId});
 export const createPatientCall = (patientId) => {
