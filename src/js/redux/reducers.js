fc3bd803ad4dcf93fc20f0f098fed61493e7b6a8
sou-c <sou.chon@outlook.com>
2018-06-11 13:13:35 +0100

    restructured reducer into smaller reducers

--- src/js/redux/reducers.js
+++ src/js/redux/reducers.js
@@ -1,14 +1,55 @@
 import initialState from './initialState.js';
+import { combineReducers } from 'redux';
 
-export const rootReducer = (state = initialState, action) => {     //need to change to immutables
+// export const rootReducer = (state = initialState, action) => {     //need to change to immutables
+//     switch (action.type) {
+//     case 'SEARCH_PATIENTS_BY_ID_REQUEST':
+//         return {...state, patientProfile: {fetching: true, data: {}}};
+//     case 'SEARCH_RESULT_BY_ID_SUCCESS':
+//         return {...state, rightPanel: 1, patientProfile: {fetching: false, data: action.payload}};
+//     case 'CLICKED_CREATE_PATIENT':
+//         return {...state, rightPanel: 2, createPatient: {patientId: action.payload}};
+//     default:
+//         return state;
+//     }
+// };
+function middlePanel(state = 1, action){
+    return state;
+}
+
+function rightPanel(state = 2, action){
+    return state;
+}
+
+function availableFields(state = initialState.availableFields, action){
+    return state;
+}
+
+function createPatient(state = {patientId: ''}, action) {
     switch (action.type) {
-    case 'SEARCH_PATIENTS_BY_ID_REQUEST':
-        return {...state, patientProfile: {fetching: true, data: {}}};
-    case 'SEARCH_RESULT_BY_ID_SUCCESS':
-        return {...state, rightPanel: 1, patientProfile: {fetching: false, data: action.payload}};
-    case 'CLICKED_CREATE_PATIENT':
-        return {...state, rightPanel: 2, createPatient: {patientId: action.payload}};
-    default:
-        return state;
+        case 'CLICKED_CREATE_PATIENT':
+            return {patientId: action.payload};
+        default:
+            return state; 
     }
-};
+}
+
+function patientProfile(state = initialState.patientProfile, action) {
+    switch (action.type) {
+        case 'SEARCH_PATIENTS_BY_ID_REQUEST':
+            return {fetching: true, data: {}};
+        case 'SEARCH_RESULT_BY_ID_SUCCESS':
+            return {fetching: false, data: action.payload};
+        default:
+            return state;
+    }
+}
+
+export const rootReducer = combineReducers({
+    middlePanel,
+    rightPanel,
+    createPatient,
+    patientProfile,
+    availableFields
+})
+
