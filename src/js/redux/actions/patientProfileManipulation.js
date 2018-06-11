1aa6fe29bac60c02fa5098bcb9509fdf64698c28
sou-c <sou.chon@outlook.com>
2018-06-11 13:32:48 +0100

    added some actions

---
+++ src/js/redux/actions/patientProfileManipulation.js
@@ -0,0 +1,25 @@
+export const addNewData = data => ({type: 'ADD_NEW_DATA', payload: data});
+
+export const updateData = data => ({type: 'UPDATE_DATA', payload: data});
+
+export const commitChangesRequest = patientId => ({type: 'COMMIT_CHANGES_REQUEST', payload: patientId});
+
+export const commitChangesSuccess = patientId => ({type: 'COMMIT_CHANGES_SUCCESS', payload: patientId});
+
+// export const commitChangesCall = (patientId) => {
+//     return dispatch => {
+//         dispatch(commitChangesRequest(patientId));
+//         // return fetch('/api/patients',{
+//         //     method: 'POST',
+//         //     mode: 'cors',
+//         //     headers: {'content-type': 'application/json',
+//         //         'token': 'd86d6e50ade67a3a0569ebc84d6041ea9bac36cb'},   //change later
+//         //     body: JSON.stringify({'alias_id': patientId, 'study': 'optimise'})
+//         })
+//             .then(res => res.json(), err => console.log(err))
+//             .then(json => {
+//                 console.log(json);
+//                 dispatch(createPatientSuccess(json))
+//             })
+//     }
+// }
