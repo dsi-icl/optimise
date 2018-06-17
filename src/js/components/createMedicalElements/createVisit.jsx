308148ee26377d02035a825543b153130ed0637f
Chon <sou.chon@icloud.com>
2018-06-17 11:50:24 +0100

    made redux create visit

--- src/js/components/createMedicalElements/createVisit.jsx
+++ src/js/components/createMedicalElements/createVisit.jsx
@@ -5,28 +5,14 @@ import moment from 'moment';
 import 'react-datepicker/dist/react-datepicker.css';
 import css from '../../../css/patientProfile.css.js';
 import {BackButton} from '../dataPage.jsx';
+import {createVisitAPICall} from '../../redux/actions/createVisit';
 
 
 class PickDate extends Component {
-    constructor (props) {
-      super(props)
-      this.state = {
-        startDate: moment()
-      };
-      this.handleChange = this.handleChange.bind(this);
-    }
-   
-    handleChange(date) {
-      this.setState({
-        startDate: date
-      });
-      console.log(this.props);
-    }
-   
     render() {
       return <DatePicker
-          selected={this.state.startDate}
-          onChange={this.handleChange}
+          selected={this.props.startDate}
+          onChange={this.props.handleChange}
       />;
     }
 }
@@ -44,21 +30,34 @@ function parseDate(dateString) {
 class CreateVisit_toConnect extends Component {
     constructor() {
         super();
-    
+        this.state = {
+            startDate: moment()
+        };
+        this._handleDateChange = this._handleDateChange.bind(this);
+        this._handleSubmitClick = this._handleSubmitClick.bind(this);
+    }
+
+    _handleDateChange(date) {
+        this.setState({
+          startDate: date
+        });
     }
 
     _handleSubmitClick() {
-        
+        const date = this.state.startDate._d;
+        const requestBody = {patientId: this.props.patientId, visitDate: {day: date.getDate(), month: date.getMonth() + 1, year: date.getFullYear()}};
+        console.log(requestBody);
+        this.props.createVisit(requestBody);
     }
 
     render() {
         return (<div>
                 <BackButton to={`/patientProfile/${this.props.patientId}`}/>
                 <h2>CREATE A NEW VISIT</h2>
-                <span style={{display: 'block', width: '60%', margin:'0 auto'}}>Please enter date on which the visit occurs / occured: <br/> <span style={{display: 'block', width: '50%', margin:'0 auto'}}><PickDate/></span> </span>
-                <div style={{cursor: 'pointer', textAlign: 'center', backgroundColor: 'lightgrey', borderRadius: 20, width: '30%', marginLeft: 'auto', marginRight: 'auto', marginTop: 15}}>Submit</div>
+                <span style={{display: 'block', width: '60%', margin:'0 auto'}}>Please enter date on which the visit occurs / occured: <br/> <span style={{display: 'block', width: '50%', margin:'0 auto'}}><PickDate startDate={this.state.startDate} handleChange={this._handleDateChange}/></span> </span>
+                <div onClick={this._handleSubmitClick} style={{cursor: 'pointer', textAlign: 'center', backgroundColor: 'lightgrey', borderRadius: 20, width: '30%', marginLeft: 'auto', marginRight: 'auto', marginTop: 15}}>Submit</div>
             </div>);
     }
 }
 
-export const CreateVisit = connect(state => ({patientId: state.patientProfile.data.patientId}))(CreateVisit_toConnect);
+export const CreateVisit = connect(state => ({patientId: state.patientProfile.data.patientId}), dispatch => ({createVisit: body => dispatch(createVisitAPICall(body))}))(CreateVisit_toConnect);
---
+++ src/js/redux/actions/createVisit.js
@@ -0,0 +1,21 @@
+import {getPatientProfileById} from './searchPatientById.js';
+import actionTypes from './listOfActions.js';
+
+const createVisitRequest = (body) => ({type: actionTypes.visits.CREATE_VISIT_REQUEST, payload: body});
+const createVisitSuccess = (json) => ({type: actionTypes.visits.CREATE_VISIT_SUCCESS, payload: json});
+
+export const createVisitAPICall = (body) => {
+    return dispatch => fetch(`/api/visits`, {
+        mode: 'cors',
+        headers: {'token': 'd86d6e50ade67a3a0569ebc84d6041ea9bac36cb', //change later
+                    'content-type': 'application/json'},  
+        method: 'POST',
+        body: JSON.stringify(body)
+    })
+    .then(res => res.text(), err => console.log(err))
+    .then(text => {
+        console.log(text);
+        console.log(body);
+        dispatch(getPatientProfileById(body.patientId));         //think about abortion later
+    })
+}
