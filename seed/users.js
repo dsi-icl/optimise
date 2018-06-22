146c43df609ce0fd195628cf6c0caf7e5451bd8c
Radiateurs <pierre-marie.danieau@epitech.eu>
2018-06-22 19:36:55 +0200

    Adding pw for admin and patching table references in visits

--- seed/users.js
+++ seed/users.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
         .then(function () {
             // Inserts seed entries
             return knex('USERS').insert([
-                { username:'admin', realName:'admin', pw:'a92e2c29eccc56ea49ed627a2656c4d5be74c29d80bd31709dd8b196a7f30825', adminPriv:1, createdByUser:1 },
+                { username:'admin', realName:'admin', pw:'$2b$12$8SfpVptBECQpcjMd/ZEACe1P6ngJfrFeBoersb8g3Payn9DzVtMaO', adminPriv:1, createdByUser:1 },
                 { username:'pm', realName:'Pierre-Marie Danieau', pw:'4e9ad5555a1a33711c74a9be2e31b38d1eb02058259fdef60a5cfa0c3216f88d', adminPriv:0, createdByUser:1 }
             ]);
         });
