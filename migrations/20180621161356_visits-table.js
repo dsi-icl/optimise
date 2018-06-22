b564717410cd677a47dc9e9c7a9a51a67d616ed8
Radiateurs <pierre-marie.danieau@epitech.eu>
2018-06-22 19:36:55 +0200

    Adding pw for admin and patching table references in visits

--- migrations/20180621161356_visits-table.js
+++ migrations/20180621161356_visits-table.js
@@ -4,7 +4,7 @@ exports.up = function(knex, Promise) {
       table.increments('id').primary();
       table.integer('patient').notNullable().references('id').inTable('PATIENTS');
       table.text('visitDate').notNullable();
-      table.integer('type').notNullable().references('id').inTable('VISIT_TYPES')
+      table.integer('type').notNullable().references('id').inTable('TYPES')
       table.text('createdTime').notNullable().defaultTo(knex.fn.now());
       table.integer('createdByUser').notNullable().references('id').inTable('USERS');
       table.text('deleted').notNullable().defaultTo('-');  
