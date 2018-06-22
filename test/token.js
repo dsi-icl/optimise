d1b83136b7836fedc12d90615fdecb93627620d9
Chon Sou <sou.chon@outlook.com>
2018-06-22 22:20:07 +0100

    fixed ceController tests

--- test/token.js
+++ test/token.js
@@ -1,5 +1,5 @@
 const adminToken = '63bbff9b42783f395c1876b3b031a02c3509ae7c';
-const standardToken = '6ef9be897d9e4a123c81f1d653065f8ed0660b88';
+const standardToken = 'd844e282491350d2939efdbb8154cf801335af3b';
 
 exports.adminToken = adminToken;
 exports.standardToken = standardToken;
