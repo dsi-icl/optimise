f0181bbe765c83ada5d45b6ab6eb9c9b20ad163f
epissari <e.pissaridou@imperial.ac.uk>
2018-06-22 07:59:30 +0100

    Adding export controller test

---
+++ test/exportCdisc.test.js
@@ -0,0 +1,24 @@
+/* global describe test expect */
+
+const app = require('../src/app');
+const request = require('supertest')(app);
+const tokens = require('./token');
+const token = tokens.token;
+const standardToken = tokens.standardToken;
+
+
+const ExportController = require('../src/controllers/exportController');
+
+describe('Export database tests', () => {
+    test('Export all demographics', () => request
+        .get('/api/exportDb')
+        //.set('token', token)
+        .then(res => {
+            expect(res.statusCode).toBe(200);
+            //expect(res.headers['content-type']).toBe('application/csv; charset=utf-8');
+            //expect(res.body.length).toBeGreaterThanOrEqual(1);
+        }));
+
+
+
+});
