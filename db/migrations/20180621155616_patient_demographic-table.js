7c7f12d0a45d9b4675fa298b272ef3fe4d87b272
Chon Sou <sou.chon@outlook.com>
2018-06-23 16:05:48 +0100

    made script to install db

--- db/migrations/20180621155616_patient_demographic-table.js
+++ db/migrations/20180621155616_patient_demographic-table.js
@@ -9,7 +9,7 @@ exports.up = function(knex, Promise) {
         table.integer('dominantHand').notNullable().references('id').inTable('DOMINANT_HANDS');
         table.integer('ethnicity').notNullable().references('id').inTable('ETHNICITIES');
         table.integer('countryOfOrigin').notNullable().references('id').inTable('COUNTRIES');
-        table.integer('alcoholUsage').notNullable().references('id').inTable('ALCOHOL_USAGES');
+        table.integer('alcoholUsage').notNullable().references('id').inTable('ALCOHOL_USAGE');
         table.integer('smokingHistory').notNullable().references('id').inTable('SMOKING_HISTORY');
         table.text('createdTime').notNullable().defaultTo(knex.fn.now());
         table.integer('createdByUser').notNullable().references('id').inTable('USERS');
