34621eedff701f76a4ada73746b64277539a5c79
Chon Sou <sou.chon@outlook.com>
2018-06-24 21:01:59 +0100

    fixing status bar position and scaling

--- src/css/globalDefault.css.js
+++ src/css/globalDefault.css.js
@@ -2,7 +2,6 @@ export default {
     app: {
         display: 'grid',
         gridTemplateColumns: '1.7fr 8fr 24fr 17fr',
-        gridTemplateRows: '90fr 2.2fr',
         color: '#363A3B',
         fontFamily: 'sans-serif',
         fontSize: 12,
