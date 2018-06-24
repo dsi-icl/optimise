e2718b4fefad18c7f9f92cfafd65088717ba313c
Chon Sou <sou.chon@outlook.com>
2018-06-24 22:35:47 +0100

    fixing typo

--- src/controllers/availableFieldController.js
+++ src/controllers/availableFieldController.js
@@ -4,7 +4,7 @@ class AvailableFieldController {
     getFields(req, res){     //bound to GETclinicalEvents and GETtestTypes too
         const tableMap = {
             'visitFields': 'AVAILABLE_FIELDS_VISITS',
-            'testFields': 'AVAILABLE_FILELDS_TESTS',
+            'testFields': 'AVAILABLE_FIELDS_TESTS',
             'clinicalEvents': 'AVAILABLE_CLINICAL_EVENT_TYPES',
             'testTypes': 'AVAILABLE_TEST_TYPES'
         };
