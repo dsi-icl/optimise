b96969099229f2c9c05c1acc04d7f1ecf8ebfe54
Chon Sou <sou.chon@outlook.com>
2018-06-24 21:01:59 +0100

    fixing status bar position and scaling

--- src/css/scaffold.css.js
+++ src/css/scaffold.css.js
@@ -6,7 +6,6 @@ export default {
         backgroundColor: '#3a3c46',
         padding: padding,
         gridColumn: '1/2',
-        gridRow: '1/2',
         position: 'relative'
     },
 
@@ -14,32 +13,31 @@ export default {
         backgroundColor: '#E7E5E6',
         padding: padding,
         gridColumn: '2/3',
-        overflow: 'auto',
-        gridRow: '1/2'
+        overflow: 'auto'
     },
 
     rightPanel: {
         backgroundColor: '#f5f6fa',
         padding: padding,
         gridColumn: '3/4',
-        overflow: 'auto',
-        gridRow: '1/2'
+        overflow: 'auto'
     },
 
     farRightPanel: {
         backgroundColor: '#E7E5E6',
         padding: padding,
         gridColumn: '4/5',
-        overflow: 'auto',
-        gridRow: '1/2'
+        overflow: 'auto'
     },
 
     statusBar: {
-        position: 'relative',
+        position: 'fixed',
         backgroundColor: '#ff5151',
-        gridColumn: '1/5',
+        width: '100%',
+        minHeight: '1.2em',
         overflow: 'auto',
-        gridRow: '2/3',
-        color: 'white'
+        color: 'white',
+        bottom: 0,
+        left: 0
     }
 }
--- src/js/App.js
+++ src/js/App.js
@@ -12,7 +12,6 @@ class App extends Component {
                 <RightPanel/>
                 <FarRightPanel/>
                 <StatusBar/>
-
             </div>
         );
     }
