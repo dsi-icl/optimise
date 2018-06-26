5c35ae8328b3d471e50596214221996922839bd4
epissari <epissari@ic.ac.uk>
2018-06-26 23:59:24 +0100

    Creating all 'non-visit' datasets

--- src/controllers/exportController.js
+++ src/controllers/exportController.js
@@ -1,6 +1,7 @@
 const knex = require('../utils/db-connection');
-const SelectorUtils = require('../utils/selector-utils');
+const selectorUtils = require('../utils/selector-utils');
 const fs = require('fs');
+const path = require('path');
 
 class ExportController {
 
@@ -10,18 +11,44 @@ class ExportController {
 
         const fileName = 'OptimiseData.csv';
 
-        // TODO: replace with using SelectorUtils to reuse the queries passing an array of all IDs
-
-        // Patient demographic data file
-        // All files should include aliasId
+        /* Patient demographic data file */
 
         knex('PATIENTS')
             .select('PATIENTS.id', 'PATIENTS.aliasId', 'PATIENTS.study', 'PATIENT_DEMOGRAPHIC.DOB', 'PATIENT_DEMOGRAPHIC.gender', 'PATIENT_DEMOGRAPHIC.dominantHand', 'PATIENT_DEMOGRAPHIC.ethnicity', 'PATIENT_DEMOGRAPHIC.countryOfOrigin')
             .leftOuterJoin('PATIENT_DEMOGRAPHIC', 'PATIENTS.id', 'PATIENT_DEMOGRAPHIC.patient')
-            .where('PATIENTS.deleted', null)
+            .where('PATIENTS.deleted', '-')
             .then(result => {
-                // TODO: CDISC mapping for headers
-                const tempfileName = `dm${fileName}`;
+                helper(result, 'dm');
+            });
+
+        /* patient medical history data */
+
+        knex('PATIENTS')
+            .select('PATIENTS.id', 'PATIENTS.aliasId', 'MEDICAL_HISTORY.relation', 'MEDICAL_HISTORY.conditionName', 'MEDICAL_HISTORY.startDate', 'MEDICAL_HISTORY.outcome', 'MEDICAL_HISTORY.resolvedYear')
+            .leftOuterJoin('MEDICAL_HISTORY', 'PATIENTS.id', 'MEDICAL_HISTORY.patient')
+            .where('PATIENTS.deleted', '-')
+            .andWhere('MEDICAL_HISTORY.deleted', '-')
+            .then(result => {
+                helper(result, 'mh');
+            });
+
+        /* patient immunisation data */
+
+        knex('PATIENTS')
+            .select('PATIENTS.id', 'PATIENTS.aliasId', 'PATIENT_IMMUNISATION.vaccineName', 'PATIENT_IMMUNISATION.immunisationDate')
+            .leftOuterJoin('PATIENT_IMMUNISATION', 'PATIENTS.id', 'PATIENT_IMMUNISATION.patient')
+            .where('PATIENTS.deleted', '-')
+            .andWhere('PATIENT_IMMUNISATION.deleted', '-')
+            .then(result => {
+                helper(result, 'immunisation');
+            });
+
+        /* result: promise, prefix: (string) filename prefix */
+
+        var helper = function(result, prefix) {
+
+            if (result.length >= 1) {
+                const tempfileName = `${prefix}${fileName}`;
                 let keys = Object.keys(result[0]); // get the keys from result to create headers
                 let tempResult = `${keys.join(',')}\n`;
                 result.forEach(function(obj) {
@@ -32,15 +59,19 @@ class ExportController {
                     tempResult += '\n';
                 });
                 let fileContents = Buffer.from(tempResult);
-                const tempSavedPath = `${__dirname}/temp/${tempfileName}`;
+                let tempSavedPath = `/temp/${tempfileName}`;
+                //tempSavedPath = path.normalize(tempSavedPath);
                 fs.writeFile(tempSavedPath, fileContents, err => {
                     if (err) {
                         throw err;
                     } else {
-                        res.status(200).download(tempSavedPath, tempfileName);
+                        //res.status(200).download(tempSavedPath, tempfileName);
                     }
                 });
-            });
+            } else {
+                res.status(204).send('There are no patient entries in the database.');
+            }
+        };
     }
 }
 
