acd8ce53036d333dc3a621ca9748eeb5e33e974a
Chon Sou <sou.chon@outlook.com>
2018-06-26 11:05:45 +0100

    some more css fix

--- src/js/components/patientProfile/sharedComponents.jsx
+++ src/js/components/patientProfile/sharedComponents.jsx
@@ -27,11 +27,11 @@ export class PatientProfileTop extends Component {
                 <span>{this.props.image}<h1 style={{ display: 'inline-block', fontSize: '1.5rem', position: 'relative', top: '1rem' }}> Patient ID: <b>{this.props.patientId}</b></h1></span>
                 <div style={{ position: 'absolute', right: '2em' }}>
                     <NavLink to={`${window.location.pathname}/createVisit`} style={{ textDecoration: 'none' }}>
-                        <div class='patientBanner UserActionButton' style={{ width: '1.7em', height: '2em', float: 'left', position: 'relative', right: 10 }}><AddVisitIcon width='1.7em'/></div>
+                        <div class='patientBanner UserActionButton' style={{ borderRadius: 5, width: '1.7em', height: '2em', float: 'left', position: 'relative', right: 10 }}><AddVisitIcon width='1.7em'/></div>
                     </NavLink>
-                    <div class='patientBanner UserActionButton' style={{ width: '1.7em', height: '2em', float: 'left' }}><AddTestIcon width='1.7em'/></div>
+                    <div class='patientBanner UserActionButton' style={{ borderRadius: '5px 0 0 5px', width: '1.7em', height: '2em', float: 'left' }}><AddTestIcon width='1.7em'/></div>
                     <div class='patientBanner UserActionButton' style={{ width: '1.7em', height: '2em', float: 'left' }}><AddTreatmentIcon width='1.7em'/></div>
-                    <div class='patientBanner UserActionButton' style={{ width: '1.7em', height: '2em', float: 'left' }}><AddEventIcon width='0.6em' style={{ height: '1.7em' }}/></div>
+                    <div class='patientBanner UserActionButton' style={{ borderRadius: '0 5px 5px 0', width: '1.7em', height: '2em', float: 'left' }}><AddEventIcon width='0.6em' style={{ height: '1.7em' }}/></div>
                 </div>
             </div>
         );
