4906db074bd08c98f32e8be8440ec3da10c843d2
epissari <epissari@ic.ac.uk>
2018-06-26 23:45:32 +0100

    Added 'deleted' to unique check in table PATIENT_IMMUNISATION

--- db/migrations/20180621155602_patient_immunisation-table.js
+++ db/migrations/20180621155602_patient_immunisation-table.js
@@ -9,7 +9,7 @@ exports.up = function(knex, Promise) {
         table.text('createdTime').notNullable().defaultTo(knex.fn.now());
         table.integer('createdByUser').notNullable().references('id').inTable('USERS');
         table.text('deleted').notNullable().defaultTo('-');
-        table.unique(['patient', 'vaccineName', 'immunisationDate']);
+        table.unique(['patient', 'vaccineName', 'immunisationDate', 'deleted']);
     });
 };
 
