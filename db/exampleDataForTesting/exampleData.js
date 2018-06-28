0cf3ab3bd8e352051c1d2737367a787ff646704c
epissari <epissari@ic.ac.uk>
2018-06-28 21:51:30 +0100

    Editing example data

--- db/exampleDataForTesting/exampleData.js
+++ db/exampleDataForTesting/exampleData.js
@@ -24,21 +24,21 @@ const exampleData = {
         { id: 4, patient: 7, visitDate: '1/1/1930', type: 1, createdByUser: 2, deleted: '-' }
     ],
     'VISIT_DATA': [
-        { id: 1, visit: 1, field: 1, value: 'blood pressure here', createdByUser: 1, deleted: 'OOOO' },
-        { id: 2, visit: 2, field: 5, value: 'weight here', createdByUser: 1, deleted: '-' },
-        { id: 3, visit: 1, field: 6, value: 'academic', createdByUser: 2, deleted: '-' },
+        { id: 1, visit: 1, field: 1, value: 150, createdByUser: 1, deleted: '1@xxxxxxxx' },
+        { id: 2, visit: 2, field: 5, value: 60, createdByUser: 1, deleted: '-' },
+        { id: 3, visit: 1, field: 6, value: 'NO', createdByUser: 2, deleted: '-' },
         { id: 4, visit: 3, field: 20, value: 'BOTH', createdByUser: 1, deleted: '-' },
         { id: 5, visit: 4, field: 18, value: 'NO', createdByUser: 1, deleted: '-' }
     ],
     'ORDERED_TESTS': [
-        { id: 1, orderedDuringVisit: 1, type: 1, expectedOccurDate: '1/2/3921', createdByUser: 1, deleted: '-' },
-        { id: 2, orderedDuringVisit: 3, type: 1, expectedOccurDate: '1/1/3921', createdByUser: 1, deleted: '-' },
-        { id: 3, orderedDuringVisit: 2, type: 1, expectedOccurDate: '1/4/3921', createdByUser: 1, deleted: '-' }
+        { id: 1, orderedDuringVisit: 1, type: 1, expectedOccurDate: '1/2/2031', createdByUser: 1, deleted: '-' },
+        { id: 2, orderedDuringVisit: 3, type: 1, expectedOccurDate: '1/1/1932', createdByUser: 1, deleted: '-' },
+        { id: 3, orderedDuringVisit: 2, type: 1, expectedOccurDate: '1/4/2031', createdByUser: 1, deleted: '-' }
     ],
     'TEST_DATA': [
-        { id: 1, test: 1, field: 5, value: 'result a', createdByUser: 1, deleted: '-' },
-        { id: 2, test: 1, field: 15, value: 'result b', createdByUser: 1, deleted: '-' },
-        { id: 3, test: 2, field: 34, value: 'result c', createdByUser: 1, deleted: '-' }
+        { id: 1, test: 1, field: 5, value: 'negative', createdByUser: 1, deleted: '-' },
+        { id: 2, test: 1, field: 15, value: 'unknown', createdByUser: 1, deleted: '-' },
+        { id: 3, test: 2, field: 34, value: 'positive', createdByUser: 1, deleted: '-' }
     ],
     'CLINICAL_EVENTS': [
         { id: 1, patient: 1, type: 1, dateStartDate: '1/4/919', createdByUser: 1, deleted: '-' },
@@ -56,17 +56,17 @@ const exampleData = {
         { id: 2, orderedDuringVisit: 3, drug: 4, dose: 2, unit: 'cc', form: 'IV', timesPerDay: 2, durationWeeks: 1, createdByUser: 2, deleted: '-' }
     ],
     'TREATMENTS_INTERRUPTIONS': [
-        { id: 1, treatment: 1, startDate: '1/1/1', endDate: '2/1/1', reason: 2, createdByUser: 1, deleted: '-' },
-        { id: 2, treatment: 1, startDate: '10/1/1', createdByUser: 1, deleted: '-' }
+        { id: 1, treatment: 1, startDate: '10/01/2017', endDate: '20/01/2018', reason: 2, createdByUser: 1, deleted: '-' },
+        { id: 2, treatment: 1, startDate: '10/1/2019', createdByUser: 1, deleted: '-' }
     ],
     'MEDICAL_HISTORY': [
         { id: 1, patient: 3, relation: 1, conditionName: 1, outcome: 'ongoing', createdByUser: 1, deleted: '-' },
-        { id: 2, patient: 4, relation: 1, conditionName: 1, outcome: 'resolved', createdByUser: 1, deleted: '@xxx' }
+        { id: 2, patient: 4, relation: 1, conditionName: 1, outcome: 'resolved', createdByUser: 1, deleted: '1@xxxxxxxx' }
     ],
     'PATIENT_IMMUNISATION': [
-        { id: 1, patient: 2, vaccineName: 'vaccine A', immunisationDate: '1/2/3921', createdByUser: 1, deleted: '-' },
-        { id: 2, patient: 3, vaccineName: 'vaccine B', immunisationDate: '1/5/3921', createdByUser: 1, deleted: 'x' },
-        { id: 3, patient: 3, vaccineName: 'vaccine C', immunisationDate: '1/6/3921', createdByUser: 1, deleted: '-' }
+        { id: 1, patient: 2, vaccineName: 'vaccine A', immunisationDate: '1/2/2018', createdByUser: 1, deleted: '-' },
+        { id: 2, patient: 3, vaccineName: 'vaccine B', immunisationDate: '1/5/2005', createdByUser: 1, deleted: '1@xxxxxxxx' },
+        { id: 3, patient: 3, vaccineName: 'vaccine C', immunisationDate: '1/6/2010', createdByUser: 1, deleted: '-' }
     ]
 };
 
