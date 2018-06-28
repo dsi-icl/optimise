396232f7bea39ff647c470344ce40e67b681dccb
Radiateurs <pierre-marie.danieau@epitech.eu>
2018-06-28 20:39:34 +0100

    Fixing missing semi-colon (Sorry about that)

--- src/controllers/demographicDataController.js
+++ src/controllers/demographicDataController.js
@@ -57,7 +57,7 @@ class DemographicDataController {
                             createEntry(req, res, 'PATIENT_DEMOGRAPHIC', entryObj, databaseErrMsg);
                         } else if (result.length !== 1) {
                             res.status(404).send('Cannot seem to find your patient!');
-                            return
+                            return ;
                         } else if (resu.length !== 0) {
                             res.status(400).send('Patient already have demographic data.');
                             return ;
