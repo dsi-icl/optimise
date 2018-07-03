f19a88b7537155863747510d3416fb381f1b0c07
epissari <e.pissaridou@imperial.ac.uk>
2018-07-03 13:13:02 +0100

    Fixed linting issues

--- src/utils/message-utils.js
+++ src/utils/message-utils.js
@@ -1,17 +1,17 @@
 const ErrorMessage = {
-    CREATIONFAIL:'Could\'t create entry',
-    DELETEFAIL:'Couldn\'t delete entry',
-    UPDATEFAIL:'Couldn\'t update entry',
-    GETFAIL:'Couldn\'t find the requested entry',
-    NOTFOUND:'Requested entry not found'
-}
+    CREATIONFAIL: 'Could\'t create entry',
+    DELETEFAIL: 'Couldn\'t delete entry',
+    UPDATEFAIL: 'Couldn\'t update entry',
+    GETFAIL: 'Couldn\'t find the requested entry',
+    NOTFOUND: 'Requested entry not found'
+};
 
 const UserRelatedErrorMessage = {
-    MISSINGARGUMENT:'The request is missing some arguments',
-    WRONGARGUMENTS:'Wrong value in the given arguments',
-    BADPASSWORD:'The password provided doesn\'t match',
-    BADCREDENTIALS:'Invalid credentials',
-    NORIGHTS:'Unauthorized to do this action'
-}
+    MISSINGARGUMENT: 'The request is missing some arguments',
+    WRONGARGUMENTS: 'Wrong value in the given arguments',
+    BADPASSWORD: 'The password provided doesn\'t match',
+    BADCREDENTIALS: 'Invalid credentials',
+    NORIGHTS: 'Unauthorized to do this action'
+};
 
-module.exports = {errorMessages:ErrorMessage, userError:UserRelatedErrorMessage};
+module.exports = { errorMessages: ErrorMessage, userError: UserRelatedErrorMessage };
