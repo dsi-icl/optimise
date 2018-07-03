37571c8fe992e81d9c707ef575f8000c86cac5c7
Chon Sou <sou.chon@outlook.com>
2018-07-03 13:25:11 +0100

    WIP create tests

---
+++ src/js/components/createMedicalElements/createTest.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Switch, Route, NavLink } from 'react-router-dom';
+
+export class CreateTest extends Component {
+    render() {
+        return <div>BooYa</div>;
+    }
+}
