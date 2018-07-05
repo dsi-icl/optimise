54194b21916cbfa9851e8a8161604325340b557b
Chon Sou <sou.chon@outlook.com>
2018-07-05 18:40:07 +0100

    made login and reshaped state; breaking a lot of dependencies, will fix

--- src/js/App.js
+++ src/js/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { MenuBar, MiddlePanel, RightPanel, FarRightPanel, StatusBar } from './components/scaffold.jsx'; 
 import css from '../css/scaffold.css';
+import { LoginPage } from './components/login/loginPage.jsx';
 require('react-datepicker/dist/react-datepicker-cssmodules.css');
 
+@connect(state => ({ loggedIn: state.login.loggedIn }))
 class App extends Component {
     render() {
-        return (
-            <div className={css.App}>
-                <MenuBar/>
-                <MiddlePanel/>
-                <RightPanel/>
-                <FarRightPanel/>
-                <StatusBar/>
-            </div>
-        );
+        if (this.props.loggedIn) {
+            return <div>HOOYA</div>;
+            // return (
+            //     <div className={css.App}>
+            //         <MenuBar/>
+            //         <MiddlePanel/>
+            //         <RightPanel/>
+            //         <FarRightPanel/>
+            //         <StatusBar/>
+            //     </div>
+            // );
+        } else {
+            return <LoginPage/>;
+        }
     }
 }
 
