fc1c88b92f09135989c86ff84840eb38699b53dc
Chon Sou <sou.chon@outlook.com>
2018-07-05 19:48:30 +0100

    made the app fetch available fields when first started

--- src/js/App.js
+++ src/js/App.js
@@ -1,17 +1,60 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { MenuBar, MiddlePanel, RightPanel, FarRightPanel, StatusBar } from './components/scaffold.jsx'; 
-import css from '../css/scaffold.css';
 import { LoginPage } from './components/login/loginPage.jsx';
+import cssLogin from '../css/loginpage.css';
+import { LoadingIcon } from '../statics/svg/icons.jsx';
+import cssIcons from '../css/icons.css';
+import cssScaffold from '../css/scaffold.css';
+import { getVisitFieldsCall, getTestFieldsCall, getClinicalEventTypesCall, getTestTypesCall } from './redux/actions/availableFields.js';
 require('react-datepicker/dist/react-datepicker-cssmodules.css');
 
+
+// WHAT IF USERS DIRECTLY GO TO A DEEP LINK ?   maybe <ROUTE render={(match)} => App/>
 @connect(state => ({ loggedIn: state.login.loggedIn }))
 class App extends Component {
+    componentWillUnmount(){
+        if (this.props.loggedIn) {
+            // dispatch logout
+        }
+    }
+
     render() {
         if (this.props.loggedIn) {
-            return <div>HOOYA</div>;
+            return <LoadingFields/>;
+        } else {
+            return <LoginPage/>;
+        }
+    }
+}
+
+
+
+function mapDispatchToProps(dispatch) {
+    return {
+        getVisitFieldsCall: () => dispatch(getVisitFieldsCall()),
+        getTestFieldsCall: () => dispatch(getTestFieldsCall()),
+        getClinicalEventTypesCall: () => dispatch(getClinicalEventTypesCall()),
+        getTestTypesCall: () => dispatch(getTestTypesCall())
+    };
+}
+
+@connect(state => ({ fetching: state.availableFields.fetching }), mapDispatchToProps)
+class LoadingFields extends Component {
+    componentDidMount() {
+        this.props.getVisitFieldsCall();
+        this.props.getTestFieldsCall();
+        this.props.getClinicalEventTypesCall();
+        this.props.getTestTypesCall();
+    }
+
+    render(){
+        if (this.props.fetching) {
+            return <div className={cssLogin.loadingFields}><div className={cssIcons.spinner}><LoadingIcon/></div> Loading project data..</div>;
+        } else {
+            return <div>GOOD</div>;
             // return (
-            //     <div className={css.App}>
+            //     <div className={cssScaffold.App}>
             //         <MenuBar/>
             //         <MiddlePanel/>
             //         <RightPanel/>
@@ -19,8 +62,6 @@ class App extends Component {
             //         <StatusBar/>
             //     </div>
             // );
-        } else {
-            return <LoginPage/>;
         }
     }
 }
