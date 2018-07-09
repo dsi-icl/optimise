352ca997509fa3b128ed7d9c23481003f67a797e
Chon Sou <sou.chon@outlook.com>
2018-07-09 15:34:07 +0100

    handle not finding a patient

--- src/js/redux/actions/searchPatientById.js
+++ src/js/redux/actions/searchPatientById.js
@@ -1,6 +1,9 @@
-export const searchPatientsByIdRequest = searchString => ({ type: 'SEARCH_PATIENTS_BY_ID_REQUEST', payload: searchString });
+import actionTypes from './listOfActions.js';
+import { promises } from 'fs';
 
-export const searchResultByIdSuccess = data => ({ type: 'SEARCH_RESULT_BY_ID_SUCCESS', payload: data });
+export const searchPatientsByIdRequest = searchString => ({ type: actionTypes.searchPatientById.SEARCH_PATIENTS_BY_ID_REQUEST, payload: searchString });
+export const searchPatientsByIdFailure = patientId => ({ type: actionTypes.searchPatientById.SEARCH_RESULT_BY_ID_FAILURE, payload: patientId });
+export const searchResultByIdSuccess = data => ({ type: actionTypes.searchPatientById.SEARCH_RESULT_BY_ID_SUCCESS, payload: data });
 
 export const getPatientProfileById = (searchString) => dispatch => {
     dispatch(searchPatientsByIdRequest(searchString));
@@ -8,7 +11,14 @@ export const getPatientProfileById = (searchString) => dispatch => {
         mode: 'cors',
         headers: { 'token': '69a87eeedcd5c90fea179a0c2464dff2f130a27a' }   //change later
     })
-        .then(res => res.json(), err => console.log(err))
+        .then(res => {
+            if (res.status === 200) {
+                return res.json();
+            } else {
+                dispatch(searchPatientsByIdFailure(searchString));
+                return Promise.reject('404');
+            }
+        }, err => console.log(err))
         .then(json => {
             console.log(json);
             dispatch(searchResultByIdSuccess(json))
