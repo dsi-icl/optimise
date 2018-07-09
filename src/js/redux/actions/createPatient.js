e74dc9e62a5c6012fa857c15250fffa05a6729c0
Chon Sou <sou.chon@outlook.com>
2018-07-09 15:22:10 +0100

    made create patient (WIP)

--- src/js/redux/actions/createPatient.js
+++ src/js/redux/actions/createPatient.js
@@ -3,18 +3,17 @@ import { getPatientProfileById } from './searchPatientById.js';
 
 export const createPatientRequest = patientId => ({ type: actionTypes.createPatients.CREATE_PATIENT_REQUEST, payload: patientId });
 export const createPatientSuccess = patientId => ({ type: actionTypes.createPatients.CREATE_PATIENT_SUCCESS, payload: patientId });
-export const createPatientCall = (patientId) => dispatch => {
-    dispatch(createPatientRequest(patientId));
-    return fetch('/api/patients', {
-        method: 'POST',
-        mode: 'cors',
-        headers: { 'content-type': 'application/json',
-            'token': '69a87eeedcd5c90fea179a0c2464dff2f130a27a' },   //change later
-        body: JSON.stringify({ 'aliasId': patientId, 'study': 'optimise' })
+export const createPatientCall = (body) => dispatch => fetch('/api/patients', {
+    method: 'POST',
+    mode: 'cors',
+    headers: { 'content-type': 'application/json',
+        'token': '69a87eeedcd5c90fea179a0c2464dff2f130a27a' },   //change later
+    body: JSON.stringify({ 'aliasId': body.patientId, 'study': 'optimise' })
+})
+    .then(res => res.json(), err => console.log(err))
+    .then(json => {
+        console.log(json);
+        dispatch(createPatientSuccess(body.patientId));
     })
-        .then(res => res.json(), err => console.log(err))
-        .then(json => {
-            console.log(json);
-            dispatch(getPatientProfileById(patientId));
-        })
-}
+
+//unfinished
