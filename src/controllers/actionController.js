ee1608b24a15dfb15f60dbd2a6f54df80a12bd8b
Radiateurs <pierre-marie.danieau@epitech.eu>
2018-07-10 15:06:34 +0100

    Adding getting action log

---
+++ src/controllers/actionController.js
@@ -0,0 +1,25 @@
+const ErrorHelper = require('../utils/error_helper');
+const message = require('../utils/message-utils');
+const ActionCore = require('../core/actionLog');
+
+function ActionCtrl() {
+    this.action = new ActionCore();
+
+    this.getLogs = ActionCtrl.prototype.getLogs.bind(this);
+}
+
+ActionCtrl.prototype.getLogs = function (req, res) {
+    if (req.requester.priv !== 1) {
+        res.status(401).json(ErrorHelper(message.userError.NORIGHTS));
+        return;
+    }
+    this.action.getLogs().then(function (result) {
+        res.status(200).json(result);
+        return;
+    }, function (error) {
+        res.status(400).json(ErrorHelper(message.errorMessages.GETFAIL, error));
+        return;
+    });
+};
+
+module.exports = ActionCtrl;
