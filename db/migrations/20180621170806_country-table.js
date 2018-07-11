f3568fa297a62158c4c9b5a450bac1a61cdc72de
Radiateurs <pierre-marie.danieau@epitech.eu>
2018-07-11 14:21:53 +0100

    Midification of country table

--- db/migrations/20180621170806_country-table.js
+++ db/migrations/20180621170806_country-table.js
@@ -1,7 +1,7 @@
 exports.up = function (knex) {
     return knex.schema.createTable('COUNTRIES', function (table) {
         table.increments('id').primary().notNullable();
-        table.text('country').notNullable();
+        table.text('value').notNullable();
     });
 };
 
