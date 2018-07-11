cc6a0c21e32751c1ec47af2e3a4b232559e9da8b
epissari <e.pissaridou@imperial.ac.uk>
2018-07-11 14:09:00 +0100

    Adding migration files

---
+++ db/migrations/20180711133737_patient_pregnancy-table.js
@@ -0,0 +1,17 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('PATIENT_PREGNANCY', function(table) {
+        table.increments('id').primary();
+        table.integer('patient').notNullable().references('id').inTable('PATIENTS');
+        table.text('startDate');
+        table.integer('outcome').notNullable().references('id').inTable('PREGNANCY_OUTCOMES');
+        table.text('outcomeDate');
+        table.text('createdTime').notNullable().defaultTo(knex.fn.now());
+        table.integer('createdByUser').notNullable().references('id').inTable('USERS');
+        table.text('deleted').notNullable().defaultTo('-');
+        table.unique(['patient', 'startDate', 'deleted']);
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('PATIENT_PREGNANCY');
+};
