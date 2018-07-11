0fd88a79462a6119a2b5e91a45ec9fd688f015b6
epissari <e.pissaridou@imperial.ac.uk>
2018-07-11 14:09:00 +0100

    Adding migration files

---
+++ db/migrations/20180711133647_patient_pii-table.js
@@ -0,0 +1,18 @@
+exports.up = function (knex) {
+    return knex.schema.createTable('PATIENT_PII', function (table) {
+        table.increments('id').primary();
+        table.integer('patient').notNullable().references('id').inTable('PATIENTS');
+        table.text('firstName').notNullable();
+        table.integer('surname').notNullable();
+        table.integer('fullAddress').notNullable();
+        table.integer('postcode').notNullable();
+        table.text('createdTime').notNullable().defaultTo(knex.fn.now());
+        table.integer('createdByUser').notNullable().references('id').inTable('USERS');
+        table.text('deleted').notNullable().defaultTo('-');
+        table.unique(['patient', 'deleted']);
+    });
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTable('PATIENT_PII');
+};
