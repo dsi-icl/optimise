dc71d5914e8ba342782b653f0951ffa7c73653b5
Chon Sou <sou.chon@outlook.com>
2018-07-11 21:04:15 +0100

    changing the mapping of country of origin to accom the new backend

--- src/js/components/createPatient/createPatientPage.jsx
+++ src/js/components/createPatient/createPatientPage.jsx
@@ -76,7 +76,7 @@ export class CreatePatientComponent extends Component {    //get these props fro
                         Gender: <SelectField name='gender' value={this.state.gender} options={genders} handler={this._handleChange}/> <br/><br/>
                         Dominant hand: <SelectField name='dominant_hand' value={this.state['dominant_hand']} options={dominant_hands} handler={this._handleChange}/> <br/><br/>
                         Ethnicity: <SelectField name='ethnicity' value={this.state['ethnicity']} options={ethnicities} handler={this._handleChange}/> <br/><br/>
-                        Country of origin: <SelectFieldCountry name='country_of_origin' value={this.state['country_of_origin']} options={countries} handler={this._handleChange}/> <br/><br/>
+                        Country of origin: <SelectField name='country_of_origin' value={this.state['country_of_origin']} options={countries} handler={this._handleChange}/> <br/><br/>
                         Alcohol usage: <SelectField name='alcohol_usage' value={this.state['alcohol_usage']} options={alcohol_usage} handler={this._handleChange}/> <br/><br/>
                         Smoking history: <SelectField name='smoking_history' value={this.state['smoking_history']} options={smoking_history} handler={this._handleChange}/> <br/><br/>
                         <input type="submit" value="Submit"/>
@@ -103,26 +103,6 @@ class SelectField extends Component {
         return (<select onChange={this.props.handler} name={this.props.name} value={this.props.value}>
             <option value={0}>unselected</option>
             {this.props.options.map(el => <option key={el.id} value={el.id}>{el.value}</option>)}
-        </select>)
-    }
-}
-
-
-
-
-/* A SEPARATE MAPPING JUST FOR COUNTRIES...BECAUSE OF INCONSISTENCY IN BACKEND..WILL CHANGE ONCE BACKEND IS PATCHED */
-
-/**
- * @prop {string} this.props.name - field name
- * @prop {string} this.props.value - value (usually passed from state of parent)
- * @prop {Array} this.props.options - list of options to be rendered; each option would be {id: x, value: 'abc'}
- * @prop {Function} this.props.handler - event listener passed down by parent to change the state of parent
- */
-class SelectFieldCountry extends Component {
-    render() {
-        return (<select onChange={this.props.handler} name={this.props.name} value={this.props.value}>
-            <option value={0}>unselected</option>
-            {this.props.options.map(el => <option key={el.id} value={el.id}>{el.country}</option>)}
         </select>)
     }
 }
