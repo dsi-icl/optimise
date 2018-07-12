ce246e978196cc8c7ed0968a6f0259186fd26c5e
Chon Sou <sou.chon@outlook.com>
2018-07-12 14:30:50 +0100

    made input datalist and fixed some formating of immunisation

---
+++ src/js/components/meDRA/meDRApicker.jsx
@@ -0,0 +1,402 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export const mockList = [
+    {
+        id: 1,
+        name: 'whatever this is'
+    },
+    {
+        id: 2,
+        name: 'whatever it was'
+    },
+    {
+        id: 3,
+        name: 'florian is a flower'
+    },
+    {
+        id: 4,
+        name: 'florian is a flore'
+    },
+    {
+        id: 5,
+        name: 'call it what you want'
+    },
+    {
+        id: 6,
+        name: 'helena beat'
+    },
+    {
+        id: 7,
+        name: 'pay the man'
+    },
+    {
+        id: 1,
+        name: 'whatever this is'
+    },
+    {
+        id: 2,
+        name: 'whatever it was'
+    },
+    {
+        id: 3,
+        name: 'florian is a flower'
+    },
+    {
+        id: 4,
+        name: 'florian is a flore'
+    },
+    {
+        id: 5,
+        name: 'call it what you want'
+    },
+    {
+        id: 6,
+        name: 'helena beat'
+    },
+    {
+        id: 7,
+        name: 'pay the man'
+    },
+    {
+        id: 1,
+        name: 'whatever this is'
+    },
+    {
+        id: 2,
+        name: 'whatever it was'
+    },
+    {
+        id: 3,
+        name: 'florian is a flower'
+    },
+    {
+        id: 4,
+        name: 'florian is a flore'
+    },
+    {
+        id: 5,
+        name: 'call it what you want'
+    },
+    {
+        id: 6,
+        name: 'helena beat'
+    },
+    {
+        id: 7,
+        name: 'pay the man'
+    },
+    {
+        id: 1,
+        name: 'whatever this is'
+    },
+    {
+        id: 2,
+        name: 'whatever it was'
+    },
+    {
+        id: 3,
+        name: 'florian is a flower'
+    },
+    {
+        id: 4,
+        name: 'florian is a flore'
+    },
+    {
+        id: 5,
+        name: 'call it what you want'
+    },
+    {
+        id: 6,
+        name: 'helena beat'
+    },
+    {
+        id: 7,
+        name: 'pay the man'
+    },
+    {
+        id: 1,
+        name: 'whatever this is'
+    },
+    {
+        id: 2,
+        name: 'whatever it was'
+    },
+    {
+        id: 3,
+        name: 'florian is a flower'
+    },
+    {
+        id: 4,
+        name: 'florian is a flore'
+    },
+    {
+        id: 5,
+        name: 'call it what you want'
+    },
+    {
+        id: 6,
+        name: 'helena beat'
+    },
+    {
+        id: 7,
+        name: 'pay the man'
+    },
+    {
+        id: 1,
+        name: 'whatever this is'
+    },
+    {
+        id: 2,
+        name: 'whatever it was'
+    },
+    {
+        id: 3,
+        name: 'florian is a flower'
+    },
+    {
+        id: 4,
+        name: 'florian is a flore'
+    },
+    {
+        id: 5,
+        name: 'call it what you want'
+    },
+    {
+        id: 6,
+        name: 'helena beat'
+    },
+    {
+        id: 7,
+        name: 'pay the man'
+    },
+    {
+        id: 1,
+        name: 'whatever this is'
+    },
+    {
+        id: 2,
+        name: 'whatever it was'
+    },
+    {
+        id: 3,
+        name: 'florian is a flower'
+    },
+    {
+        id: 4,
+        name: 'florian is a flore'
+    },
+    {
+        id: 5,
+        name: 'call it what you want'
+    },
+    {
+        id: 6,
+        name: 'helena beat'
+    },
+    {
+        id: 7,
+        name: 'pay the man'
+    },
+    {
+        id: 1,
+        name: 'whatever this is'
+    },
+    {
+        id: 2,
+        name: 'whatever it was'
+    },
+    {
+        id: 3,
+        name: 'florian is a flower'
+    },
+    {
+        id: 4,
+        name: 'florian is a flore'
+    },
+    {
+        id: 5,
+        name: 'call it what you want'
+    },
+    {
+        id: 6,
+        name: 'helena beat'
+    },
+    {
+        id: 7,
+        name: 'pay the man'
+    },
+    {
+        id: 1,
+        name: 'whatever this is'
+    },
+    {
+        id: 2,
+        name: 'whatever it was'
+    },
+    {
+        id: 3,
+        name: 'florian is a flower'
+    },
+    {
+        id: 4,
+        name: 'florian is a flore'
+    },
+    {
+        id: 5,
+        name: 'call it what you want'
+    },
+    {
+        id: 6,
+        name: 'helena beat'
+    },
+    {
+        id: 7,
+        name: 'pay the man'
+    },
+    {
+        id: 1,
+        name: 'whatever this is'
+    },
+    {
+        id: 2,
+        name: 'whatever it was'
+    },
+    {
+        id: 3,
+        name: 'florian is a flower'
+    },
+    {
+        id: 4,
+        name: 'florian is a flore'
+    },
+    {
+        id: 5,
+        name: 'call it what you want'
+    },
+    {
+        id: 6,
+        name: 'helena beat'
+    },
+    {
+        id: 7,
+        name: 'pay the man'
+    },
+    {
+        id: 1,
+        name: 'whatever this is'
+    },
+    {
+        id: 2,
+        name: 'whatever it was'
+    },
+    {
+        id: 3,
+        name: 'florian is a flower'
+    },
+    {
+        id: 4,
+        name: 'florian is a flore'
+    },
+    {
+        id: 5,
+        name: 'call it what you want'
+    },
+    {
+        id: 6,
+        name: 'helena beat'
+    },
+    {
+        id: 7,
+        name: 'pay the man'
+    },
+    {
+        id: 1,
+        name: 'whatever this is'
+    },
+    {
+        id: 2,
+        name: 'whatever it was'
+    },
+    {
+        id: 3,
+        name: 'florian is a flower'
+    },
+    {
+        id: 4,
+        name: 'florian is a flore'
+    },
+    {
+        id: 5,
+        name: 'call it what you want'
+    },
+    {
+        id: 6,
+        name: 'helena beat'
+    },
+    {
+        id: 7,
+        name: 'pay the man'
+    },
+    {
+        id: 1,
+        name: 'whatever this is'
+    },
+    {
+        id: 2,
+        name: 'whatever it was'
+    },
+    {
+        id: 3,
+        name: 'florian is a flower'
+    },
+    {
+        id: 4,
+        name: 'florian is a flore'
+    },
+    {
+        id: 5,
+        name: 'call it what you want'
+    },
+    {
+        id: 6,
+        name: 'helena beat'
+    },
+    {
+        id: 7,
+        name: 'pay the man'
+    }
+];
+
+export const numOfReturns = 3;
+
+/**
+ * @class SuggestionInput
+ * @prop {Array} this.props.possibleValues
+ * @prop {Number} this.props.numOfReturns
+ */
+export class SuggestionInput extends Component {
+    constructor() {
+        super();
+        this.state = { value: '' };
+        this._handleKeyPress = this._handleKeyPress.bind(this);
+    }
+
+    _handleKeyPress(ev){
+        console.log(ev.target);
+        this.setState({ 
+            value: ev.target.value
+        }, () => { console.log(this.state) });
+    }
+
+    render(){
+        return (
+            <div>
+                <input type='text' onChange={this._handleKeyPress} value={this.state.value} list='datalist'/>
+                <datalist id='datalist'>
+                    {this.props.possibleValues.map(el => <option key={el.id} value={el.name}/>)}
+                </datalist>
+            </div>
+        );
+    }
+}
