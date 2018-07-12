2f64300ab7687666e4943ac6dfc06955e9cf6528
Radiateurs <pierre-marie.danieau@epitech.eu>
2018-07-12 07:09:07 +0100

    Finxing lint error

--- src/controllers/availableFieldController.js
+++ src/controllers/availableFieldController.js
@@ -7,7 +7,7 @@ class AvailableFieldController {
         const tableMap = {
             'visitFields': 'AVAILABLE_FIELDS_VISITS',
             'testFields': 'AVAILABLE_FIELDS_TESTS',
-            'ceFields':'AVAILABLE_FIELDS_CE',
+            'ceFields': 'AVAILABLE_FIELDS_CE',
             'clinicalEvents': 'AVAILABLE_CLINICAL_EVENT_TYPES',
             'testTypes': 'AVAILABLE_TEST_TYPES'
         };
@@ -19,7 +19,7 @@ class AvailableFieldController {
             let table = tableMap[req.params.dataType];
             knex(table)
                 .select('*')
-                .where(moduleObj).then(function(result){
+                .where(moduleObj).then(function (result) {
                     res.status(200).json(result);
                     return;
                 }, function (error) {
