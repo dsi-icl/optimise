c98b34fce05bbd0b215fcff55a09f5db228b6031
Chon Sou <sou.chon@outlook.com>
2018-07-12 16:50:23 +0100

    abstracted api call to apihelper

--- src/js/redux/actions/createVisit.js
+++ src/js/redux/actions/createVisit.js
@@ -1,30 +1,15 @@
 import { getPatientProfileById } from './searchPatientById.js';
 import actionTypes from './listOfActions.js';
-import { promises } from 'fs';
+import { apiHelper } from '../fetchHelper.js';
 
 const createVisitRequest = (body) => ({ type: actionTypes.visits.CREATE_VISIT_REQUEST, payload: body });
 const createVisitSuccess = (json) => ({ type: actionTypes.visits.CREATE_VISIT_SUCCESS, payload: json });
 
 export const createVisitAPICall = (body) => dispatch => {
     console.log(body);
-    return fetch('/visits', {
-        mode: 'cors',
-        headers: {
-            'token': '69a87eeedcd5c90fea179a0c2464dff2f130a27a', //change later
-            'content-type': 'application/json'
-        },
-        method: 'POST',
-        body: JSON.stringify(body.data)
-    })
-        .then(res => {
-            if (res.status === 200) {
-                return res.text();
-            } else {
-                return Promise.reject(res);
-            }
-        }, err => console.log(err))
+    return apiHelper('/visits', { method: 'POST', body: JSON.stringify(body.data) })
         .then(text => {
-            dispatch(getPatientProfileById(body.patientId));         //think about abortion later    //and think about not having to refresh the whole page
+            dispatch(getPatientProfileById(body.patientId));
         })
         .catch(res => res.text().then(msg => console.log(msg)));
 }
--- src/js/redux/actions/tests.js
+++ src/js/redux/actions/tests.js
@@ -1,25 +1,10 @@
 import { getPatientProfileById } from './searchPatientById.js';
-import actionTypes from './listOfActions.js';
+import { apiHelper } from '../fetchHelper.js';
 
 export const createTestAPICall = (body) => dispatch => {
     console.log(body);
-    return fetch('/tests', {
-        mode: 'cors',
-        headers: {
-            'token': '69a87eeedcd5c90fea179a0c2464dff2f130a27a', //change later
-            'content-type': 'application/json'
-        },
-        method: 'POST',
-        body: JSON.stringify(body.data)
-    })
-        .then(res => {
-            if (res.status === 200) {
-                return 0;
-            } else {
-                return '';
-            }
-        }, err => console.log(err))
+    return apiHelper('/tests', { method: 'POST', body: JSON.stringify(body.data) })
         .then(json => {
-            dispatch(getPatientProfileById(body.patientId));         //think about abortion later    //and think about not having to refresh the whole page
+            dispatch(getPatientProfileById(body.patientId));
         })
 }
