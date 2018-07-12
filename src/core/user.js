a878dc86994dbf5269c308c73135c8a00693fd1e
Florian Guitton <f.guitton@imperial.ac.uk>
2018-07-12 18:16:39 +0100

    Fixes #75

--- src/core/user.js
+++ src/core/user.js
@@ -11,18 +11,18 @@ function User() {
     this.updateUser = User.prototype.updateUser.bind(this);
 }
 
-User.prototype.getUser = function(user) {
-    return new Promise(function(resolve, reject) {
-        knex('USERS').select({ id: 'id', username: 'username', realname: 'realname' }).where('username', 'like', user).then(function(result){
+User.prototype.getUser = function (user) {
+    return new Promise(function (resolve, reject) {
+        knex('USERS').select({ id: 'id', username: 'username', realname: 'realname' }).where('username', 'like', user).then(function (result) {
             resolve(result);
-        }, function(error){
+        }, function (error) {
             reject(ErrorHelper(message.errorMessages.GETFAIL, error));
         });
     });
 };
 
-User.prototype.createUser = function(requester, user) {
-    return new Promise(function(resolve, reject){
+User.prototype.createUser = function (requester, user) {
+    return new Promise(function (resolve, reject) {
         let entryObj = {};
         entryObj.username = user.username;
         if (user.hasOwnProperty('realName'))
@@ -30,38 +30,40 @@ User.prototype.createUser = function(requester, user) {
         entryObj.pw = bcrypt.hashSync(user.pw, saltRound);
         entryObj.adminPriv = user.isAdmin;
         entryObj.createdByUser = requester.userid;
-        createEntry('USERS', entryObj).then(function(result){
+        createEntry('USERS', entryObj).then(function (result) {
             resolve(result);
-        }, function(error){
+        }, function (error) {
             reject(ErrorHelper(message.errorMessages.CREATIONFAIL, error));
         });
     });
 };
 
-User.prototype.updateUser = function(requester, user) {
-    return new Promise(function(resolve, reject){
+User.prototype.updateUser = function (requester, user) {
+    return new Promise(function (resolve, reject) {
         let hashed = bcrypt.hashSync(user.pw, saltRound);
-        updateEntry('USERS', requester, '*', { 'username': user.username }, { pw: hashed }).then(function(result){
+        updateEntry('USERS', requester, '*', { 'username': user.username }, { pw: hashed }).then(function (result) {
             resolve(result);
-        }, function(error){
+        }, function (error) {
             reject(ErrorHelper(message.errorMessages.UPDATEFAIL, error));
         });
     });
 };
 
-User.prototype.deleteUser = function(requester, userId) {
-    return new Promise(function(resolve, reject){
-        deleteEntry('USERS', requester, userId).then(function(result){
+User.prototype.deleteUser = function (requester, userId) {
+    return new Promise(function (resolve, reject) {
+        deleteEntry('USERS', requester, userId).then(function (result) {
             resolve(result);
-        }, function(error){
+        }, function (error) {
             reject(ErrorHelper(message.errorMessages.DELETEFAIL, error));
         });
     });
 };
 
-User.prototype.loginUser = function(user) {
-    return new Promise(function(resolve, reject){
-        getEntry('USERS', { username: user.username }, { pw: 'pw', id: 'id' }).then(function(result){
+User.prototype.loginUser = function (user) {
+    return new Promise(function (resolve, reject) {
+        getEntry('USERS', { username: user.username }, { pw: 'pw', id: 'id' }).then(function (result) {
+            if (result.length !== 1)
+                reject(ErrorHelper(message.errorMessages.GETFAIL));
             try {
                 if (!bcrypt.compareSync(user.pw, result[0].pw)) {
                     reject(ErrorHelper(message.userError.BADPASSWORD, new Error(message.userError.WRONGARGUMENTS)));
@@ -73,22 +75,22 @@ User.prototype.loginUser = function(user) {
             let entryObj = {};
             entryObj.user = result[0].id;
             entryObj.sessionToken = token;
-            createEntry('USER_SESSION', entryObj).then(function(__unused__result){
+            createEntry('USER_SESSION', entryObj).then(function (__unused__result) {
                 resolve(token);
-            }, function(error){
+            }, function (error) {
                 reject(ErrorHelper(message.errorMessages.CREATIONFAIL, error));
             });
-        }, function(error){
+        }, function (error) {
             reject(ErrorHelper(message.errorMessages.GETFAIL, error));
         });
     });
 };
 
-User.prototype.logoutUser = function(requester) {
-    return new Promise(function(resolve, reject) {
-        deleteEntry('USER_SESSION', requester, { sessionToken: requester.token }).then(function(result){
+User.prototype.logoutUser = function (requester) {
+    return new Promise(function (resolve, reject) {
+        deleteEntry('USER_SESSION', requester, { sessionToken: requester.token }).then(function (result) {
             resolve(result);
-        },function(error){
+        }, function (error) {
             reject(ErrorHelper(message.errorMessages.DELETEFAIL, error));
         });
     });
