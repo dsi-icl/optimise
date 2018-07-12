81bdd848276d014f4ceb3cf9eb4585d340c85419
Radiateurs <pierre-marie.danieau@epitech.eu>
2018-07-13 00:27:01 +0100

    Adding meddra getter

---
+++ src/controllers/meddraController.js
@@ -0,0 +1,48 @@
+const knex = require('../utils/db-connection');
+const ErrorHelper = require('../utils/error_helper');
+const message = require('../utils/message-utils');
+
+let MeddraCollection = null;
+
+function Meddra() {
+    let that = this;
+    this.MeddraCollection = null;
+    this.getMeddraField = Meddra.prototype.getMeddraField.bind(this);
+    this.setMeddraCollection = Meddra.prototype.setMeddraCollection.bind(this);
+    knex('ADVERSE_EVENT_MEDDRA').select('*').then(function (result) {
+        that.setMeddraCollection(result);
+    }, function (error) {
+        that.setMeddraCollection(null);
+    });
+}
+
+Meddra.prototype.setMeddraCollection = function(collection) {
+    this.MeddraCollection = collection;
+}
+
+Meddra.prototype.getMeddraField = function(req, res) {
+    let result = [];
+    if (this.MeddraCollection === null) {
+        res.status(400).json(ErrorHelper(message.errorMessages.GETFAIL));
+        return;
+    }
+    if (req.query.hasOwnProperty('search')) {
+        let j = 0;
+        result = this.MeddraCollection.filter(obj => obj.name.indexOf(req.query.search) > -1)
+
+        // for (let i = 0; i < this.MeddraCollection.length; i++) {
+        //     if (this.MeddraCollection[i].name.indexOf(req.query.search) > -1) {
+        //         result[j] = this.MeddraCollection[i];
+        //         j++;
+        //     }
+        // }
+        res.status(200).json(result);
+        return;
+    }
+    else {
+        res.status(200).json(this.MeddraCollection);
+        return;
+    }
+}
+
+module.exports = Meddra;
