15ac549e4b6d07d0ac455102b60573c8d723a65b
epissari <e.pissaridou@imperial.ac.uk>
2018-07-12 05:13:58 +0100

    Creating a separate table for Serious Adverse Events

--- schema/schema.sql
+++ schema/schema.sql
@@ -211,7 +211,6 @@ CREATE TABLE TREATMENTS (
     durationWeeks NUMERIC NOT NULL CHECK (durationWeeks > 0),
     terminatedDate TEXT,
     terminatedReason TEXT CHECK (terminatedReason IN ('patient preference','disease progresssion', 'death', 'life threatening reaction to drug', 'permanent / serious disability', 'prolonged hospitalization')),
-    adverseEvent INTEGER REFERENCES ADVERSE_EVENT_MEDDRA(id),
     createdTime TEXT NOT NULL DEFAULT (datetime('now')),
     createdByUser INTEGER NOT NULL REFERENCES USERS(id),
     deleted TEXT, /*NULL or deletion time*/
@@ -232,6 +231,17 @@ CREATE TABLE TREATMENTS_INTERRUPTIONS (
     createdByUser INTEGER NOT NULL REFERENCES USERS(id),
     deleted TEXT, /*NULL or deletion time*/
     UNIQUE(treatment, startDate, deleted)
+);
+
+CREATE TABLE SERIOUS_ADVERSE_EVENTS (
+    id INTEGER PRIMARY KEY ASC,
+    treatment INTEGER NOT NULL REFERENCES TREATMENTS(id),
+    adverseEventDate TEXT NOT NULL,
+    reason INTEGER NOT NULL REFERENCES ADVERSE_EVENT_MEDDRA(id) /* reason code from MedDRA */
+    createdTime TEXT NOT NULL DEFAULT (datetime('now')),
+    createdByUser INTEGER NOT NULL REFERENCES USERS(id),
+    deleted TEXT, /*NULL or deletion time*/
+    UNIQUE(treatment, adverseEventDate, reason, deleted)
 );
 
 CREATE TABLE ADVERSE_EVENT_MEDDRA (
