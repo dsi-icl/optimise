1b4fc289cc2bb4d796e946d060c2d7dcfa69d822
Chon Sou <sou.chon@outlook.com>
2018-07-13 23:47:07 +0100

    updated the consent api call to accommate changes on the backend

--- src/js/redux/actions/consent.js
+++ src/js/redux/actions/consent.js
@@ -2,7 +2,7 @@ import { getPatientProfileById } from './searchPatientById.js';
 import { apiHelper } from '../fetchHelper.js';
 
 export const updateConsentAPICall = (body) => dispatch => {
-    return apiHelper('/consent', { method: 'POST', body: JSON.stringify(body.data) })
+    return apiHelper('/patients', { method: 'PUT', body: JSON.stringify(body.data) })
         .then(json => {
             dispatch(getPatientProfileById(body.patientId));
         })
