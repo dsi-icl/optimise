f9f05185b4f1cd9a63e423391c555806cc91c3fb
Chon Sou <sou.chon@outlook.com>
2018-07-13 11:11:33 +0100

    made log in with cookies

--- src/js/redux/actions/login.js
+++ src/js/redux/actions/login.js
@@ -1,6 +1,21 @@
 import actionTypes from './listOfActions.js';
 import { apiHelper } from '../fetchHelper.js';
 
+export const checkingLogin = () => ({ type: actionTypes.login.CHECKING_LOGIN });
+
+export const loggedIn = () => ({ type: actionTypes.login.LOGGED_IN });
+
+export const notLoggedIn = () => ({ type: actionTypes.login.NOT_LOGGED_IN });
+
+export const whoami = (body) => dispatch => {
+    dispatch(checkingLogin());
+    return apiHelper('/whoami')
+        .then(() => {
+            dispatch(loggedIn());
+        })
+        .catch(() => { dispatch(notLoggedIn()); });
+};
+
 export const loginRequest = () => ({ type: actionTypes.login.LOGIN_REQUESTED });
 
 export const loginSuccess = (body) => ({ type: actionTypes.login.LOGIN_SUCCESS, payload: body });
