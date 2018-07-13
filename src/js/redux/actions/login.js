0746804cd7c589c6b5ded84be500b322eff75107
Chon Sou <sou.chon@outlook.com>
2018-07-13 11:26:40 +0100

    finessed the login

--- src/js/redux/actions/login.js
+++ src/js/redux/actions/login.js
@@ -3,27 +3,29 @@ import { apiHelper } from '../fetchHelper.js';
 
 export const checkingLogin = () => ({ type: actionTypes.login.CHECKING_LOGIN });
 
-export const loggedIn = () => ({ type: actionTypes.login.LOGGED_IN });
+export const loggedIn = body => ({ type: actionTypes.login.LOGGED_IN, payload: body });
 
 export const notLoggedIn = () => ({ type: actionTypes.login.NOT_LOGGED_IN });
 
 export const whoami = (body) => dispatch => {
     dispatch(checkingLogin());
     return apiHelper('/whoami')
-        .then(() => {
-            dispatch(loggedIn());
+        .then(json => {
+            console.debug('LOGGEDIN > ', json);
+            dispatch(loggedIn(json[0]));
         })
         .catch(() => { dispatch(notLoggedIn()); });
 };
 
-export const loginRequest = () => ({ type: actionTypes.login.LOGIN_REQUESTED });
+export const loginRequest = body => ({ type: actionTypes.login.LOGIN_REQUESTED, payload: body });
 
 export const loginSuccess = (body) => ({ type: actionTypes.login.LOGIN_SUCCESS, payload: body });
 
 export const loginFailure = () => ({ type: actionTypes.login.LOGIN_FAILURE });
 
 export const loginAPICall = (body) => dispatch => {
-    dispatch(loginRequest());
+    console.log('BODY > ', body);
+    dispatch(loginRequest(body));
     return apiHelper('/users/login', { method: 'POST', body: JSON.stringify(body) })
         .then(json => {
             dispatch(loginSuccess(json));
--- src/js/redux/initialState.js
+++ src/js/redux/initialState.js
@@ -54,7 +54,6 @@ export default {
         initialCheckingStatus: true,
         loggingIn: false,
         loginFailed: false,
-        username: '',
-        realname: ''
+        username: ''
     }
 };
