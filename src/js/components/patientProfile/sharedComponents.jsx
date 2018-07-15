b2beb3c532480d3530a80d171ca61c1efded258a
Florian Guitton <f.guitton@imperial.ac.uk>
2018-07-15 02:48:44 +0100

    Adding close button to patient profile

--- src/js/components/patientProfile/sharedComponents.jsx
+++ src/js/components/patientProfile/sharedComponents.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { VisitPicker } from './popup';
 import Icon from '../icon';
+import { BackButton } from '../medicalData/dataPage';
 import cssDropdowns from '../../../css/dropdowns.module.css';
 
 export class PatientProfileSectionScaffold extends Component {
@@ -26,6 +27,7 @@ export class PatientProfileTop extends Component {
                 <span title='Order test' className={[cssDropdowns.dropDownMenu]} ><Icon symbol='addTest' /><VisitPicker elementType='test' /></span>
                 <span title='Add prescription' className={[cssDropdowns.dropDownMenu].join(' ')} ><Icon symbol='addTreatment' /><VisitPicker elementType='treatment' /></span>
                 <span title='Record event' className={[cssDropdowns.dropDownMenu].join(' ')} ><Icon symbol='addEvent' /><VisitPicker elementType='clinicalEvent' /></span>
+                <BackButton to={'/searchPatientById'} />
             </div>
         );
     }
