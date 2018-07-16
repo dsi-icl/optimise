5b71be096f34a8a53d3fb452d4327ee394dcb8cb
Florian Guitton <f.guitton@imperial.ac.uk>
2018-07-16 19:23:22 +0100

    Using in-memory database for tests

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ const path = require('path');
 module.exports = {
     client: 'sqlite3',
     connection: {
-        filename: path.normalize('./db/optimise-db.sqlite')
+        filename: process.env.NODE_ENV === 'test' ? ':memory:' : path.normalize('./db/optimise-db.sqlite')
     },
     migrations: {
         directory: path.normalize(`${path.dirname(__filename)}/db/migrations`)
