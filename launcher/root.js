d46eb97180e56c87f9c854bdee074e2336933597
Florian Guitton <f.guitton@imperial.ac.uk>
2018-07-17 13:57:15 +0100

    Adding compression for production server

--- launcher/root.js
+++ launcher/root.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const compression = require('compression');
 const OptimiseServer = require('optimise-core');
 const path = require('path');
 const config = require('../config/optimise.config')
@@ -7,18 +8,23 @@ let root = express();
 let optimise = new OptimiseServer(config);
 
 optimise.start().then(function (router) {
-    // linking optimise's router on /api
+
+    // For production activating reponse compression
+    root.use(compression());
+
+    // Linking optimise's router on /api
     root.use('/api', router);
+
+    // Binding static resources folder
     root.use(express.static('../build'));
+
     // Referencing any other requests to the /public/index.html 
     root.use('/*', function (req, res, next) {
-        if (req.originalUrl === '/api')
-            next();
-        else
-            res.sendFile(path.resolve('build/index.html'));
+        res.sendFile(path.resolve('build/index.html'));
     });
+
     root.listen(config.port, function (error) {
-        if (error != undefined || error != null) {
+        if (error !== undefined || error !== null) {
             console.error(error); // eslint-disable-line no-console
             return;
         }
