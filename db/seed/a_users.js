d8a5f9704b07ada4304c977aa8b36049d782cb11
Radiateurs <pierre-marie.danieau@epitech.eu>
2018-07-18 15:05:28 +0100

    Seed users with dynamic encryption

--- db/seed/a_users.js
+++ db/seed/a_users.js
@@ -1,11 +1,21 @@
+const crypto = require('crypto');
+
 exports.seed = function (knex) {
     // Deletes ALL existing entries
+    let saltAdmin = crypto.randomBytes(32).toString('base64');
+    let iterationAdmin = Number.parseInt(crypto.randomBytes(2).toString('hex'), 16);
+    let hashedAdmin = crypto.pbkdf2Sync('admin', saltAdmin, iterationAdmin, 64, 'sha512');
+    hashedAdmin = hashedAdmin.toString('base64');
+    let saltUser = crypto.randomBytes(32).toString('base64');
+    let iterationUser = Number.parseInt(crypto.randomBytes(2).toString('hex'), 16);
+    let hashedUser = crypto.pbkdf2Sync('admin', saltUser, iterationUser, 64, 'sha512');
+    hashedUser = hashedUser.toString('base64');
     return knex('USERS').del()
         .then(function () {
             // Inserts seed entries
             return knex('USERS').insert([
-                { id: 1, username: 'admin', realName: 'Administrator', pw: 'ZTBfTmM80TDowVs5ymCD/KfSrMeaz9bYt2cB9lIdyw763z5qgYqgsbGzsY9XNocw7qppmFRaBGooxmgSPCM7Ug==', salt: '1OJ3YQeOU4UOAftW7VugpxLyjoJEChmHV2wCxlh7qN2WL40hsAyA27MEKD9yCKsUyX0INWVtxxBYEvIw4Gr11b71QIdOYTSRqWuNSONThTXwCvtAw4c9yCaQO0KdQpyq', iterations: 40000, adminPriv: 1, createdByUser: 1 }, //pw: 'admin'
-                { id: 2, username: 'user', realName: 'Standard User', pw: 'ZTBfTmM80TDowVs5ymCD/KfSrMeaz9bYt2cB9lIdyw763z5qgYqgsbGzsY9XNocw7qppmFRaBGooxmgSPCM7Ug==', salt: '1OJ3YQeOU4UOAftW7VugpxLyjoJEChmHV2wCxlh7qN2WL40hsAyA27MEKD9yCKsUyX0INWVtxxBYEvIw4Gr11b71QIdOYTSRqWuNSONThTXwCvtAw4c9yCaQO0KdQpyq', iterations: 40000, adminPriv: 0, createdByUser: 1 }, //pw: 'admin'
+                { id: 1, username: 'admin', realName: 'Administrator', pw: hashedAdmin, salt: saltAdmin, iterations: iterationAdmin, adminPriv: 1, createdByUser: 1 }, //pw: 'admin'
+                { id: 2, username: 'user', realName: 'Standard User', pw: hashedUser, salt: saltUser, iterations: iterationUser, adminPriv: 0, createdByUser: 1 }, //pw: 'admin'
             ]);
         });
 };
