67a5e2eb0aaa13a6fa3f063c37542dc8f1dfcc85
Radiateurs <pierre-marie.danieau@epitech.eu>
2018-07-18 01:34:46 +0100

    Patching crypto DEFAULT ENCODING

--- db/seed/a_users.js
+++ db/seed/a_users.js
@@ -4,8 +4,8 @@ exports.seed = function (knex) {
         .then(function () {
             // Inserts seed entries
             return knex('USERS').insert([
-                { id: 1, username: 'admin', realName: 'Administrator', pw: '4a3084716784b1b5fdeb5ed04def8ba34a2a38d7fa1b7664e4d5f92b6ad31d55d1bafcffcd8dc440b30d6f300838d0a81745d77a587b4a30e06fced18d8f6b20', adminPriv: 1, createdByUser: 1 }, //pw: 'admin'
-                { id: 2, username: 'user', realName: 'Standard User', pw: '4a3084716784b1b5fdeb5ed04def8ba34a2a38d7fa1b7664e4d5f92b6ad31d55d1bafcffcd8dc440b30d6f300838d0a81745d77a587b4a30e06fced18d8f6b20', adminPriv: 0, createdByUser: 1 }, //pw: 'admin'
+                { id: 1, username: 'admin', realName: 'Administrator', pw: 'SjCEcWeEsbX9617QTe+Lo0oqONf6G3Zk5NX5K2rTHVXRuvz/zY3EQLMNbzAIONCoF0XXelh7SjDgb87RjY9rIA==', adminPriv: 1, createdByUser: 1 }, //pw: 'admin'
+                { id: 2, username: 'user', realName: 'Standard User', pw: 'SjCEcWeEsbX9617QTe+Lo0oqONf6G3Zk5NX5K2rTHVXRuvz/zY3EQLMNbzAIONCoF0XXelh7SjDgb87RjY9rIA==', adminPriv: 0, createdByUser: 1 }, //pw: 'admin'
             ]);
         });
 };
--- src/core/user.js
+++ src/core/user.js
@@ -1,7 +1,7 @@
 const { getEntry, createEntry, deleteEntry, eraseEntry } = require('../utils/controller-utils');
 const ErrorHelper = require('../utils/error_helper');
 const crypto = require('crypto');
-crypto.DEFAULT_ENCODING = 'hex';
+//crypto.DEFAULT_ENCODING = 'hex';
 const { saltRound, iteration } = require('../config/hashKeyConfig');
 const message = require('../utils/message-utils');
 const knex = require('../utils/db-connection');
@@ -40,9 +40,10 @@ User.prototype.getUserByID = function (uid) {
 User.prototype.createUser = function (userReq, user) {
     return new Promise(function (resolve, reject) {
         let entryObj = {};
+        let hashed = crypto.pbkdf2Sync(user.pw, saltRound, iteration, 64, 'sha512');
         entryObj.username = user.username;
         entryObj.realname = user.realname;
-        entryObj.pw = crypto.pbkdf2Sync(user.pw, saltRound, iteration, 64, 'sha512');
+        entryObj.pw = hashed.toString('base64');
         entryObj.adminPriv = user.isAdmin;
         entryObj.createdByUser = userReq.id;
         createEntry('USERS', entryObj).then(function (result) {
@@ -55,11 +56,17 @@ User.prototype.createUser = function (userReq, user) {
 
 User.prototype.updateUser = function (user) {
     return new Promise(function (resolve, reject) {
-        knex('USERS').update({ 'pw': crypto.pbkdf2Sync(user.pw, saltRound, iteration, 64, 'sha512') }).where({ username: user.username, deleted: '-' }).then(function (result) {
-            resolve(result);
-        }, function (error) {
+        try {
+            let hashed = crypto.pbkdf2Sync(user.pw, saltRound, iteration, 64, 'sha512');
+            knex('USERS').update({ 'pw': hashed.toString('base64') }).where({ username: user.username, deleted: '-' }).then(function (result) {
+                resolve(result);
+            }, function (error) {
+                reject(ErrorHelper(message.errorMessages.UPDATEFAIL, error));
+            });
+        } catch (err) {
             reject(ErrorHelper(message.errorMessages.UPDATEFAIL, error));
-        });
+            return;
+        }
     });
 };
 
@@ -88,11 +95,16 @@ User.prototype.loginUser = function (user) {
         getEntry('USERS', { username: user.username }, { pw: 'pw', id: 'id', username: 'username', priv: 'adminPriv' }).then(function (result) {
             if (result.length <= 0)
                 reject(ErrorHelper(message.errorMessages.GETFAIL));
-            let crypted = crypto.pbkdf2Sync(user.pw, saltRound, iteration, 64, 'sha512');
-            if (crypted !== result[0].pw)
-                reject(ErrorHelper(message.userError.BADPASSWORD, new Error(message.userError.WRONGARGUMENTS)));
-            else
-                resolve(result[0]);
+            try {
+                let crypted = crypto.pbkdf2Sync(user.pw, saltRound, iteration, 64, 'sha512');
+                if (crypted.toString('base64') !== result[0].pw)
+                    reject(ErrorHelper(message.userError.BADPASSWORD, new Error(message.userError.WRONGARGUMENTS)));
+                else
+                    resolve(result[0]);
+            } catch (err) {
+                reject(err);
+                return;
+            }
         }, function (error) {
             reject(ErrorHelper(message.errorMessages.GETFAIL, error));
         });
