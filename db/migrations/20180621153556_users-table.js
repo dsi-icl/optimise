9612a2dc4ff91c809838ab806d13e232c1f1dfc9
Radiateurs <pierre-marie.danieau@epitech.eu>
2018-07-18 14:51:56 +0100

    Patching salt inconsistency

--- db/migrations/20180621153556_users-table.js
+++ db/migrations/20180621153556_users-table.js
@@ -4,6 +4,8 @@ exports.up = function (knex) {
         table.text('username').notNullable();
         table.text('realname').notNullable();
         table.text('pw').notNullable();
+        table.text('salt').notNullable();
+        table.integer('iterations').notNullable();
         table.integer('adminPriv').notNullable();
         table.timestamp('createdTime').notNullable().defaultTo(knex.fn.now());
         table.integer('createdByUser').notNullable().references('id').inTable('USERS');
