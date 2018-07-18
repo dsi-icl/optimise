5d2190c8580efcae00dc225b254ab0afa26e9dc3
Radiateurs <pierre-marie.danieau@epitech.eu>
2018-07-18 18:44:43 +0100

    Patching lint

--- src/utils/generate-crypto.js
+++ src/utils/generate-crypto.js
@@ -1,13 +1,13 @@
 const crypto = require('crypto');
 
 function generateSaltIteration() {
-    var salt = crypto.randomBytes(32).toString('base64');
-    var iteration = Number.parseInt(crypto.randomBytes(2).toString('hex'), 16);
+    let salt = crypto.randomBytes(32).toString('base64');
+    let iteration = Number.parseInt(crypto.randomBytes(2).toString('hex'), 16);
     return { salt: salt, iteration: iteration };
 }
 
 function generateAndHash(secret) {
-    var saltitContainer = generateSaltIteration();
+    let saltitContainer = generateSaltIteration();
     let hashed = crypto.pbkdf2Sync(secret, saltitContainer.salt, saltitContainer.iteration, 64, 'sha512');
 
     hashed = hashed.toString('base64');
