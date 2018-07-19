73b01e4af2ee971701e6c78aa5adb3accce272ab
Chon Sou <sou.chon@outlook.com>
2018-07-19 17:06:26 +0100

    the tests are working (just the boilerplate)

--- __tests__/app.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { App } from '../src/js/application';
-
-
-describe('App suite', () => {
-    it('renders in dom without crashing', () => {
-        const el = mount(<App/>);
-    });
-});
