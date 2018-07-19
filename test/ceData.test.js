ea76a49cc9f3c39b26b14b5936f3f47fced95b50
Radiateurs <pierre-marie.danieau@epitech.eu>
2018-07-19 23:59:37 +0100

    Adding tests on dataController + improving data controller behaviour (WIP)

---
+++ test/ceData.test.js
@@ -0,0 +1,101 @@
+/* global describe test expect */
+
+const request = require('supertest');
+const admin = request.agent(global.optimiseRouter);
+const user = request.agent(global.optimiseRouter);
+const message = require('../src/utils/message-utils');
+const { connectAdmin, connectUser, disconnectAgent } = require('./connection');
+
+beforeAll(async () => { //eslint-disable-line no-undef
+    await connectAdmin(admin);
+    await connectUser(user);
+});
+
+afterAll(async () => { //eslint-disable-line no-undef
+    await disconnectAgent(admin);
+    await disconnectAgent(user);
+});
+
+describe('Creating CE data', () => {
+    test('Request creation without body', () => admin
+        .post('/data/clinicalEvent').then(res => {
+            expect(res.status).toBe(400);
+            expect(typeof res.body).toBe('object');
+            expect(res.body.error).toBeDefined();
+            expect(res.body.error).toBe(`${message.dataMessage.MISSINGVALUE  }clinicalEventId`);
+        }));
+
+    test('Request creation without add or update', () => admin
+        .post('/data/clinicalEvent')
+        .send({ clinicalEventId: 1 })
+        .then(res => {
+            expect(res.status).toBe(400);
+            expect(typeof res.body).toBe('object');
+            expect(res.body.error).toBeDefined();
+            expect(res.body.error).toBe(`${message.dataMessage.MISSINGVALUE  }clinicalEventId`);
+        }));
+
+    test('Request creation without clinicalEvent id', () => admin
+        .post('/data/clinicalEvent')
+        .send({ add: { 5: 'BOTH' } })
+        .then(res => {
+            expect(res.status).toBe(400);
+            expect(typeof res.body).toBe('object');
+            expect(res.body.error).toBeDefined();
+            expect(res.body.error).toBe(`${message.dataMessage.MISSINGVALUE  }clinicalEventId`);
+        }));
+
+    test('Request creation with invalid value for id', () => admin
+        .post('/data/clinicalEvent')
+        .send({ clinicalEventId: 99, add: { 1: 'YES' } })
+        .then(res => {
+            expect(res.status).toBe(404);
+            expect(typeof res.body).toBe('object');
+            expect(res.body.error).toBeDefined();
+            expect(res.body.error).toBe(message.dataMessage.CLINICALEVENT);
+        }));
+
+    test('Request creation with invalid field', () => admin
+        .post('/data/clinicalEvent')
+        .send({ clinicalEventId: 1, add: { 534567: 'BOTH' } })
+        .then(res => {
+            expect(res.status).toBe(400);
+            expect(typeof res.body).toBe('object');
+            expect(res.body.error).toBeDefined();
+            expect(res.body.error).toBe(message.dataMessage.FIELDNOTFOUND);
+        }));
+
+    test('Request creation with invalid value for requested field', () => admin
+        .post('/data/clinicalEvent')
+        .send({ clinicalEventId: 1, add: {} })
+        .then(res => {
+            expect(res.status).toBe(400);
+            expect(typeof res.body).toBe('object');
+            expect(res.body.error).toBeDefined();
+            expect(res.body.error).toBe(message.dataMessage.FIELDNOTFOUND);
+        }));
+
+    test('Request creation succesfull', () => user
+        .post('/data/clinicalEvent')
+        .send({ clinicalEventId: 1, add: { 1: 'YES' } })
+        .then(res => {
+            expect(res.status).toBe(200);
+            expect(typeof res.body).toBe('object');
+            expect(res.body.success).toBeDefined();
+            expect(res.body.message).toBeDefined();
+            expect(res.body.success).toBe(true);
+            expect(res.body.message).toBe(message.dataMessage.SUCESS);
+        }));
+
+    test('Request update succesfull', () => admin
+        .post('/data/clinicalEvent')
+        .send({ clinicalEventId: 1, update: { 1: 'NO' } })
+        .then(res => {
+            expect(res.status).toBe(200);
+            expect(typeof res.body).toBe('object');
+            expect(res.body.success).toBeDefined();
+            expect(res.body.message).toBeDefined();
+            expect(res.body.success).toBe(true);
+            expect(res.body.message).toBe(message.dataMessage.SUCESS);
+        }));
+});
