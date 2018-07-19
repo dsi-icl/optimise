e511d3a9e031a8d3f6c42c9bc26214bd2884e85b
Florian Guitton <f.guitton@imperial.ac.uk>
2018-07-19 10:18:21 +0100

    Correcting spelling

--- test/testControler.test.js
+++ test/testControler.test.js
@@ -4,7 +4,7 @@ const request = require('supertest');
 const admin = request.agent(global.optimiseRouter);
 const user = request.agent(global.optimiseRouter);
 const message = require('../src/utils/message-utils');
-const { connectAdmin, connectUser, deconnectAgent } = require('./connection');
+const { connectAdmin, connectUser, disconnectAgent } = require('./connection');
 
 beforeAll(async () => { //eslint-disable-line no-undef
     await connectAdmin(admin);
@@ -12,8 +12,8 @@ beforeAll(async () => { //eslint-disable-line no-undef
 });
 
 afterAll(async () => { //eslint-disable-line no-undef
-    await deconnectAgent(admin);
-    await deconnectAgent(user);
+    await disconnectAgent(admin);
+    await disconnectAgent(user);
 });
 
 let createdTestId;
