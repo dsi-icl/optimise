a31fb80883dd4ad7f89470424f481969b56a4aac
Chon Sou <sou.chon@outlook.com>
2018-07-20 11:32:50 +0100

    made edit demographic

--- src/js/components/editMedicalElements/router.jsx
+++ src/js/components/editMedicalElements/router.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { EditTest, EditCE, EditMed } from './index';
+import { EditTest, EditCE, EditMed, EditDemo } from './index';
 
 export class EditElementRouter extends Component {
     render() {
@@ -9,6 +9,7 @@ export class EditElementRouter extends Component {
                 <Route path='/patientProfile/:patientId/edit/test/:elementId' render={({ match, location }) => <EditTest match={match} location={location}/>} />
                 <Route path='/patientProfile/:patientId/edit/clinicalEvent/:elementId' render={({ match, location }) => <EditCE match={match} location={location}/>} />
                 <Route path='/patientProfile/:patientId/edit/treatment/:elementId' render={({ match, location }) => <EditMed match={match} location={location}/>} />
+                <Route path='/patientProfile/:patientId/edit/demographic/data' render={({ match, location }) => <EditDemo match={match} location={location}/>} />
                 <Route path='/' component={() => <></>} />
             </Switch>
         );
