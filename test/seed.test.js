d883ba7ea45cf5d236f948509b72009edb34b571
Radiateurs <pierre-marie.danieau@epitech.eu>
2018-07-20 15:09:42 +0100

    Merging seedController

---
+++ test/seed.test.js
@@ -0,0 +1,38 @@
+/* global beforeAll afterAll describe test expect */
+
+const request = require('supertest');
+const admin = request.agent(global.optimiseRouter);
+const user = request.agent(global.optimiseRouter);
+const message = require('../src/utils/message-utils');
+const { connectAdmin, connectUser, disconnectAgent } = require('./connection');
+
+beforeAll(async () => {
+    await connectAdmin(admin);
+    await connectUser(user);
+});
+
+afterAll(async () => {
+    await disconnectAgent(admin);
+    await disconnectAgent(user);
+});
+
+describe('Getting seeds', () => {
+    test('Getting not existing fields', () => admin
+        .get('/seeds/availableNotValid')
+        .then(res => {
+            expect(res.status).toBe(400);
+            expect(typeof res.body).toBe('object');
+            expect(res.body.error).toBeDefined();
+            expect(res.body.error).toBe(message.userError.WRONGPATH);
+        }));
+
+    test('Getting a field with invalid query', () => admin
+        .get('/seeds/fieldVisit?test=WRONG')
+        .then(res => {
+            expect(res.status).toBe(400);
+            expect(typeof res.body).toBe('object');
+            expect(res.body.error).toBeDefined();
+            expect(res.body.error).toBe(message.errorMessages.GETFAIL);
+        }));
+
+})
