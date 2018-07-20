a09e4c735d12dda00e1a33e7c970eba3afc26749
Florian Guitton <f.guitton@imperial.ac.uk>
2018-07-20 08:33:10 +0100

    Showing the far right Export panel by default

--- src/js/components/scaffold/farRightPanel.jsx
+++ src/js/components/scaffold/farRightPanel.jsx
@@ -24,11 +24,10 @@ export default class FarRightPanel extends Component {
                     <Route path='/patientProfile/:patientId/createVisit' render={({ match }) => <CreateVisit match={match} />} />
                     <Route path='/patientProfile/:patientId' render={({ match }) => <Section match={match} />} />
                     <Route exact path='/searchPatient/from/deletionSuccessful' component={() => <h3>The patient has been successfully deleted!</h3>} />
-                    <Route exact path='/searchPatient/' render={({ match }) => <ExportSets match={match} />} />
                     <Route exact path='/createPatient/:patientIdCreated' render={({ match }) => <CreatePatient match={match} />} />
                     <Route exact path='/filterPatients' component={() => <></>} />
                     <Route exact path='/administration/users/:userId' render={({ match }) => <UserDetail match={match} />} />
-                    <Route path='/' component={() => <></>} />
+                    <Route path='/' render={({ match }) => <ExportSets match={match} />} />
                 </Switch>
             </div>
         );
