610b2c3ec6dedf25386205d1f5afb05edcbe253d
Radiateurs <pierre-marie.danieau@epitech.eu>
2018-07-20 12:12:21 +0100

    Adding seed route

---
+++ src/routes/seedRoute.js
@@ -0,0 +1,18 @@
+/**
+ * Route seed
+ * @description Redirect request from /seeds to the proper controller call
+ */
+
+const express = require('express');
+const seed = express();
+
+const SeedController = require('../controllers/seedController');
+const SeedCtrl = new SeedController();
+// Interacts with the patients in the DB
+seed.route('/:target')
+    .get(SeedCtrl.getSeed)
+    .post(SeedCtrl.createSeed)
+    .put(SeedCtrl.editSeed)
+    .delete(SeedCtrl.deleteSeed);
+
+module.exports = seed;
