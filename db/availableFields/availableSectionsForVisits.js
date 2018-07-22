82a2abfbc1cb23225896b9944de1a7abc1d729a1
epissari <e.pissaridou@imperial.ac.uk>
2018-07-22 14:59:20 +0100

    Added batch insert for Available Visit Fields and corrected minor typos

--- db/availableFields/availableSectionsForVisits.js
+++ db/availableFields/availableSectionsForVisits.js
@@ -1,8 +1,8 @@
 const availableVisitSections = [
-    { id: 1, name: 'Vital Signs' },
-    { id: 2, name: 'Symptoms' },
-    { id: 3, name: 'Signs' },
-    { id: 4, name: 'Performance Measures' }
+    { name: 'Vital Signs' },
+    { name: 'Symptoms' },
+    { name: 'Signs' },
+    { name: 'Performance Measures' }
 ];
 
 module.exports = availableVisitSections;
