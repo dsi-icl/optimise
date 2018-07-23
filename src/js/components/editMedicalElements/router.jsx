6ac2c2414ef31f638651aaff2a12231b98fab6a9
Chon Sou <sou.chon@outlook.com>
2018-07-23 17:04:02 +0100

    added communication routing and template

--- src/js/components/editMedicalElements/router.jsx
+++ src/js/components/editMedicalElements/router.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { EditTest, EditCE, EditMed, EditDemo, EditDiagnosis } from './index';
+import { EditTest, EditCE, EditMed, EditDemo, EditDiagnosis, EditCommunication } from './index';
 
 export class EditElementRouter extends Component {
     render() {
@@ -11,6 +11,7 @@ export class EditElementRouter extends Component {
                 <Route path='/patientProfile/:patientId/edit/treatment/:elementId' render={({ match, location }) => <EditMed match={match} location={location}/>} />
                 <Route path='/patientProfile/:patientId/edit/demographic/data' render={({ match, location }) => <EditDemo match={match} location={location}/>} />
                 <Route path='/patientProfile/:patientId/edit/diagnosis/data' render={({ match, location }) => <EditDiagnosis match={match} location={location}/>} />
+                <Route path='/patientProfile/:patientId/edit/communication/:visitId' render={({ match, location }) => <EditCommunication match={match} location={location}/>} />
                 <Route path='/' component={() => <></>} />
             </Switch>
         );
