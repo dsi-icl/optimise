76ca292d1168c30c5ea239d717367a477b7c5b3a
Radiateurs <pierre-marie.danieau@epitech.eu>
2018-07-23 01:40:06 +0100

    Updating seed files after request

--- src/utils/load-json.js
+++ src/utils/load-json.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
-writeJson = function(data, pathToFile) {
+const writeJson = function (data, pathToFile) {
     fs.writeFileSync(path.resolve(pathToFile), JSON.stringify(data));
-}
+};
 
-readJson = function(pathToFile) {
+const readJson = function (pathToFile) {
     let content = fs.readFileSync(path.resolve(pathToFile));
     return (JSON.parse(content));
-}
+};
 
-module.exports = { writeJson, readJson }
+module.exports = { writeJson: writeJson, readJson: readJson };
