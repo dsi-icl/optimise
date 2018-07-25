0b46dab45690bc2b4c0fd94c90c417b63be4e8e3
Radiateurs <pierre-marie.danieau@epitech.eu>
2018-07-25 19:42:17 +0200

    Fix #135 closes #135

--- src/controllers/visitController.js
+++ src/controllers/visitController.js
@@ -3,6 +3,9 @@ const ErrorHelper = require('../utils/error_helper');
 const message = require('../utils/message-utils');
 const formatToJSON = require('../utils/format-response');
 
+/**
+ * @class VisitController: Inspect entry body and user right before sending to core
+ */
 function VisitController() {
     this.visit = new visitCore();
 
@@ -10,9 +13,18 @@ function VisitController() {
     this.createVisit = VisitController.prototype.createVisit.bind(this);
     this.deleteVisit = VisitController.prototype.deleteVisit.bind(this);
     this.updateVisit = VisitController.prototype.updateVisit.bind(this);
+    this.getReportOfVisit = VisitController.prototype.getReportOfVisit.bind(this);
+    this.createReport = VisitController.prototype.createReport.bind(this);
+    this.deleteReport = VisitController.prototype.deleteReport.bind(this);
+    this.updateReport = VisitController.prototype.updateReport.bind(this);
 }
 
-VisitController.prototype.getVisitsOfPatient = function (req, res) {
+/**
+ * @function createReport
+ * @description Create a new report
+ * @param {Object} req Request Object
+ * @param {Object} res Response Object
+ */VisitController.prototype.getVisitsOfPatient = function (req, res) {
     if (!req.query.hasOwnProperty('patientId')) {
         res.status(400).json(ErrorHelper(message.userError.MISSINGARGUMENT));
         return;
@@ -64,6 +76,112 @@ VisitController.prototype.deleteVisit = function (req, res) {
     }, function (error) {
         res.status(400).json(ErrorHelper(message.errorMessages.DELETEFAIL, error));
     });
+};
+
+/**
+ * @function getReportOfVisit
+ * @description get report depending on the query
+ * @param {Object} req Request Object
+ * @param {Object} res Response Object
+ */
+VisitController.prototype.getReportOfVisit = function (req, res) {
+    let whereObj = {};
+    if (req.query.hasOwnProperty('id')) {
+        whereObj.visit = req.query.id;
+    }
+    this.visit.getReport(whereObj).then(function (result) {
+        res.status(200).json(formatToJSON(result));
+        return;
+    }, function (error) {
+        res.status(400).json(ErrorHelper(message.errorMessages.GETFAIL, error));
+        return;
+    });
+};
+
+/**
+ * @function createReport
+ * @description Create a new report
+ * @param {Object} req Request Object
+ * @param {Object} res Response Object
+ */
+VisitController.prototype.createReport = function (req, res) {
+    if (req.body.hasOwnProperty('visit') && req.body.hasOwnProperty('report') &&
+        typeof req.body.visit === 'number' && typeof req.body.report === 'string') {
+        let newEntry = {};
+        newEntry.visit = req.body.visit;
+        newEntry.report = req.body.report;
+        newEntry.createdByUser = req.user.id;
+        this.visit.createReport(newEntry).then(function (result) {
+            res.status(200).json(formatToJSON(result));
+            return;
+        }, function (error) {
+            res.status(400).json(ErrorHelper(message.errorMessages.CREATIONFAIL, error));
+            return;
+        });
+    }
+    else if (!(req.body.hasOwnProperty('visit') && req.body.hasOwnProperty('report'))) {
+        res.status(400).json(ErrorHelper(message.userError.MISSINGARGUMENT));
+        return;
+    } else {
+        res.status(400).json(ErrorHelper(message.userError.WRONGARGUMENTS));
+        return;
+    }
+};
+
+/**
+ * @function updateReport
+ * @description Update a new report
+ * @param {Object} req Request Object
+ * @param {Object} res Response Object
+ */
+VisitController.prototype.updateReport = function (req, res) {
+    if (req.user.priv !== 1) {
+        res.status(401).json(ErrorHelper(message.userError.NORIGHTS));
+        return;
+    }
+    if (req.body.hasOwnProperty('id') && typeof req.body.id === 'number') {
+        this.visit.updateReport(req.user, req.body).then(function (result) {
+            res.status(200).json(formatToJSON(result));
+            return;
+        }, function (error) {
+            res.status(400).json(ErrorHelper(message.errorMessages.UPDATEFAIL, error));
+            return;
+        });
+    } else if (!req.body.hasOwnProperty('id')) {
+        res.status(400).json(ErrorHelper(message.userError.MISSINGARGUMENT));
+        return;
+    } else {
+        res.status(400).json(ErrorHelper(message.userError.WRONGARGUMENTS));
+        return;
+    }
+};
+
+/**
+ * @function deleteReport
+ * @description Delete a new report
+ * @param {Object} req Request Object
+ * @param {Object} res Response Object
+ */
+VisitController.prototype.deleteReport = function (req, res) {
+    if (req.user.priv !== 1) {
+        res.status(401).json(ErrorHelper(message.userError.NORIGHTS));
+        return;
+    }
+    if (req.body.hasOwnProperty('id') && typeof req.body.id === 'number') {
+        this.visit.deleteReport(req.user, req.body.id).then(function (result) {
+            res.status(200).json(formatToJSON(result));
+            return;
+        }, function (error) {
+            res.status(400).json(ErrorHelper(message.errorMessages.UPDATEFAIL, error));
+            return;
+        });
+    } else if (!req.body.hasOwnProperty('id')) {
+        res.status(400).json(ErrorHelper(message.userError.MISSINGARGUMENT));
+        return;
+    } else {
+        res.status(400).json(ErrorHelper(message.userError.WRONGARGUMENTS));
+        return;
+    }
 };
 
 module.exports = VisitController;
