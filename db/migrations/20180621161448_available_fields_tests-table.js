0c1cae0f78e013ef11f70dc36ba073bbd0f77f70
epissari <e.pissaridou@imperial.ac.uk>
2018-07-25 22:02:13 +0100

    Added properties to available fields + Minor edits

--- db/migrations/20180621161448_available_fields_tests-table.js
+++ db/migrations/20180621161448_available_fields_tests-table.js
@@ -3,10 +3,13 @@ exports.up = function (knex) {
         table.increments('id').primary();
         table.text('definition').notNullable();
         table.text('idname').notNullable();
+        table.text('section').nullable();
+        table.text('subsection').nullable();
         table.integer('type').notNullable().references('id').inTable('TYPES');
         table.text('unit').nullable();
         table.text('module').nullable();
         table.text('permittedValues').nullable();
+        table.text('labels').nullable();
         table.integer('referenceType').notNullable().references('id').inTable('AVAILABLE_TEST_TYPES');
         table.text('laterality').nullable();
         table.text('cdiscName').nullable();
