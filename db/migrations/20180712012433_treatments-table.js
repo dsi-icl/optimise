82f988c6f3c87ac8c6cba30ff1523a513a7a3b82
epissari <e.pissaridou@imperial.ac.uk>
2018-07-26 13:44:51 +0100

    Changed to nullable Treatment dose, unit and form

--- db/migrations/20180712012433_treatments-table.js
+++ db/migrations/20180712012433_treatments-table.js
@@ -3,9 +3,9 @@ exports.up = function (knex) {
         table.increments('id').primary();
         table.integer('orderedDuringVisit').notNullable().references('id').inTable('VISITS').onDelete('CASCADE');
         table.integer('drug').notNullable().references('id').inTable('AVAILABLE_DRUGS');
-        table.integer('dose').notNullable();
-        table.text('unit').notNullable();
-        table.text('form').notNullable();
+        table.integer('dose').nullable();
+        table.text('unit').nullable();
+        table.text('form').nullable();
         table.integer('timesPerDay').notNullable();
         table.text('terminatedDate').nullable();
         table.integer('terminatedReason').nullable().references('id').inTable('REASONS');
