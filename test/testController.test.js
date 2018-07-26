7811f23f03fbe13c3371f2dece555166f176a29a
epissari <e.pissaridou@imperial.ac.uk>
2018-07-26 23:37:16 +0100

    Edited testController.test

--- test/testController.test.js
+++ test/testController.test.js
@@ -81,7 +81,7 @@ describe('Create test controller tests', () => {
             expect(res.status).toBe(200);
             expect(typeof res.body).toBe('object');
             expect(res.body.state).toBeDefined();
-            expect(res.body.state).toBe(4);
+            expect(res.body.state).toBe(5);
         }));
 
     test('Request creation with good body (should succeed)', () => admin
@@ -91,7 +91,7 @@ describe('Create test controller tests', () => {
             expect(res.status).toBe(200);
             expect(typeof res.body).toBe('object');
             expect(res.body.state).toBeDefined();
-            expect(res.body.state).toBe(5);
+            expect(res.body.state).toBe(6);
         }));
 });
 
