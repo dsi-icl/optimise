feb31e95f45c1ee183764e4ae5a0503e09155133
Chon Sou <sou.chon@outlook.com>
2018-07-30 16:33:35 +0100

    adding crude date to mini-timeline

--- src/js/components/patientProfile/timeline.jsx
+++ src/js/components/patientProfile/timeline.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import Icon from '../icon';
 import { PatientProfileSectionScaffold } from './sharedComponents.jsx';
 import style from './patientProfile.module.css';
+import { Set } from '../../../../node_modules/immutable';
 
 /*
 Timeline:
@@ -25,15 +26,40 @@ export class TimelineBox extends Component {   //unfinsihed
                 allCEDates.push(el.endDate);
             }
         });
-        const allDates = [...allVisitDates, ...allTestDates, ...allTreatmentDates, ...allCEDates];
+        const allDates = [...allVisitDates, ...allTestDates, ...allTreatmentDates, ...allCEDates].map(el => parseInt(el));
         allDates.sort();
-        const daySpan = parseInt(((parseInt(allDates[allDates.length - 1], 10) - parseInt(allDates[0], 10)) / 86400000), 10);
+        console.log('ALL DATES', allDates);
+        const daySpan = parseInt(((allDates[allDates.length - 1] - allDates[0]) / 86400000), 10);
         const numOfCols = `10% ${'1fr '.repeat(daySpan + 3)}`;
         const TimelineDynamicStyle = {
             gridTemplateColumns: numOfCols,
-            gridTemplateRows: '1em 0.5em 1em 0.5em 1em 0.5em 1em'
+            gridTemplateRows: '1em 0.5em 1em 0.5em 1em 0.5em 1em 0.5em 1em'
 
         };
+        const months = {
+            0: 'jan',
+            1: 'feb',
+            2: 'mar',
+            3: 'apr',
+            4: 'may',
+            5: 'jun',
+            6: 'jul',
+            7: 'aug',
+            8: 'sep',
+            9: 'oct',
+            10: 'nov',
+            11: 'dec'
+        };
+
+
+        const mappingDateFunction = date => {
+            const ratio = parseInt((date - allDates[0]) / 86400000, 10);
+            return (
+                <div key={date} style={{ fontSize: 8, gridColumn: `${ratio + 3}/${ratio + 4}`, gridRow: '9', overflow: 'visible' }}>{`${months[new Date(parseInt(date)).getMonth()]} ${new Date(parseInt(date)).getFullYear()}`}</div>
+            );
+        };
+
+
         const mappingVisitFunction = visit => {
             const date = visit.visitDate;
             const ratio = parseInt((date - allDates[0]) / 86400000, 10);
@@ -43,6 +69,7 @@ export class TimelineBox extends Component {   //unfinsihed
                 </a>
             );
         };
+
         const mappingTestFunction = test => {
             const date = test.expectedOccurDate;
             const ratio = parseInt((date - allDates[0]) / 86400000, 10);
@@ -52,6 +79,7 @@ export class TimelineBox extends Component {   //unfinsihed
                 </a>
             );
         };
+
         const mappingMedFunction = med => {
             const date = med.startDate;
             const ratio = parseInt((date - allDates[0]) / 86400000, 10);
@@ -94,12 +122,34 @@ export class TimelineBox extends Component {   //unfinsihed
                     <div style={{ gridColumn: '1/2', gridRow: '7', overflow: 'hidden' }}>
                         Events
                     </div>
+                    <div style={{ gridColumn: '1/2', gridRow: '9', overflow: 'hidden' }}>
+                        
+                    </div>
                     {this.props.data.visits.filter(el => el.type === 1).map(mappingVisitFunction)}
                     {this.props.data.tests.map(mappingTestFunction)}
                     {this.props.data.treatments.map(mappingMedFunction)}
                     {this.props.data.clinicalEvents.map(mappingCEFunction)}
+                    {formatDate(allDates).map(mappingDateFunction)}
                 </div>
             </PatientProfileSectionScaffold>
         );
     }
+}
+
+
+function formatDate(allDates) {
+    const oneday = 86400000;
+    const ds = new Set(allDates);
+    const d = Array.from(ds.values());
+    if (d.length < 4) {
+        return d;
+    }
+    const allYears = d.map(el => new Date(el).getFullYear());
+    const ys = new Set(allYears);
+    const y = Array.from(ys.values());
+    const firstInstances = Array(d.length).fill(false);
+    for (let i = 0; i < y.length; i++) {
+        firstInstances[allYears.indexOf(y[i])] = true;
+    }
+    return d.filter((el, ind) => firstInstances[ind]);
 }
