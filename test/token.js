34f7073a9658f781e36d1d0f68bf79e0651811b1
Florian Guitton <f.guitton@imperial.ac.uk>
2018-08-01 16:21:12 +0100

    Removing old token mechanics and adding user privileges as part of the login reponse

--- test/token.js
+++
@@ -1,5 +0,0 @@
-const adminToken = '69a87eeedcd5c90fea179a0c2464dff2f130a27a';
-const standardToken = 'd844e282491350d2939efdbb8154cf801335af3b';
-
-exports.adminToken = adminToken;
-exports.standardToken = standardToken;
