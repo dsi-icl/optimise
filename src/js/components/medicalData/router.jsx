2dd908009b7311978dff154374a372dfe94160cb
Florian Guitton <f.guitton@imperial.ac.uk>
2018-08-01 13:54:53 +0100

    Separating the signs ans symptoms and applying changes to the communication panel

--- src/js/components/medicalData/router.jsx
+++ src/js/components/medicalData/router.jsx
@@ -13,7 +13,8 @@ export class DataPageRouter extends Component {
         return (
             <Switch>
                 <Route path='/patientProfile/:patientId/data/test/:testId' render={({ match }) => <TestData elementType='test' match={match} />} />
-                <Route path='/patientProfile/:patientId/data/visit/:visitId' render={({ match }) => <VisitData elementType='visit' match={match} />} />
+                <Route path='/patientProfile/:patientId/data/visit/:visitId/symptoms' render={({ match }) => <VisitData elementType='visit' match={match} category={'symptoms'} />} />
+                <Route path='/patientProfile/:patientId/data/visit/:visitId/signs' render={({ match }) => <VisitData elementType='visit' match={match} category={'signs'} />} />
                 <Route path='/patientProfile/:patientId/data/treatment/:elementId' render={({ match }) => <TreatmentInterruption match={match} />} />
                 <Route path='/patientProfile/:patientId/data/clinicalEvent/:ceId' render={({ match }) => <CeData elementType='clinicalEvent' match={match} />} />
                 <Route path='/' component={() => <></>} />
