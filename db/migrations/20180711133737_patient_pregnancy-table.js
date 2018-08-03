7a8de4e2099fb606b047f14897e0a0a790a53d72
Florian Guitton <f.guitton@imperial.ac.uk>
2018-08-03 16:20:50 +0100

    Pregnancy table changes. Outcome shouldn't be compulsory. This is a breaking change. SQLite does not support column alter

--- db/migrations/20180711133737_patient_pregnancy-table.js
+++ db/migrations/20180711133737_patient_pregnancy-table.js
@@ -1,10 +1,10 @@
-exports.up = function(knex) {
-    return knex.schema.createTable('PATIENT_PREGNANCY', function(table) {
+exports.up = function (knex) {
+    return knex.schema.createTable('PATIENT_PREGNANCY', function (table) {
         table.increments('id').primary();
         table.integer('patient').notNullable().references('id').inTable('PATIENTS').onDelete('CASCADE');
-        table.text('startDate');
-        table.integer('outcome').notNullable().references('id').inTable('PREGNANCY_OUTCOMES');
-        table.text('outcomeDate');
+        table.text('startDate').notNullable();
+        table.integer('outcome').nullable().references('id').inTable('PREGNANCY_OUTCOMES');
+        table.text('outcomeDate').nullable();
         table.integer('meddra').nullable().references('id').inTable('ADVERSE_EVENT_MEDDRA');
         table.text('createdTime').notNullable().defaultTo(knex.fn.now());
         table.integer('createdByUser').notNullable().references('id').inTable('USERS');
@@ -13,6 +13,6 @@ exports.up = function(knex) {
     });
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
     return knex.schema.dropTable('PATIENT_PREGNANCY');
 };
