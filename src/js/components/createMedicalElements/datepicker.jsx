bb6a5693d14dc288eb7d73fa61014cba1375952e
Florian Guitton <f.guitton@imperial.ac.uk>
2018-08-05 13:45:15 +0100

    Adding datepicker prompt on data fields with new data type

--- src/js/components/createMedicalElements/datepicker.jsx
+++ src/js/components/createMedicalElements/datepicker.jsx
@@ -4,20 +4,35 @@ import DatePicker from 'react-datepicker';
 
 export class PickDate extends Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+        this.state = {
+            selected: props.startDate ? props.startDate : undefined
+        }
         this._handleDateChange = this._handleDateChange.bind(this);
     }
 
     _handleDateChange(date) {
-        this.props.handleChange(moment(date));
+        let time = date ? moment(date) : undefined;
+        time = time && time.isValid() ? time : undefined;
+        this.setState({
+            selected: time ? time : undefined
+        }, () => {
+            if (this.props.handleChange)
+                this.props.handleChange(this.state.selected);
+        });
     }
 
     render() {
-        return <DatePicker
-            selected={this.props.startDate}
-            onChange={this._handleDateChange}
-            dateFormat="DD/MM/YYYY"
-            style={{ width: '100%' }} />;
+        return (
+            <>
+                <DatePicker
+                    selected={this.state.selected}
+                    onChange={this._handleDateChange}
+                    dateFormat="DD/MM/YYYY"
+                    style={{ width: '100%' }} />
+                {this.props.handleChange === undefined ? <input style={{ display: 'none' }} type='test' value={this.state.selected ? this.state.selected.toISOString() : ''} ref={this.props.reference} readOnly /> : null}
+            </>
+        );
     }
 }
