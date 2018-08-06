6136027caccad304f1a1cb1d4d226e9f86280d77
Florian Guitton <f.guitton@imperial.ac.uk>
2018-08-06 17:57:24 +0100

    Fixing CE creation to render meddra optional

--- src/js/components/createMedicalElements/createCE.jsx
+++ src/js/components/createMedicalElements/createCE.jsx
@@ -18,7 +18,7 @@ export class CreateCE extends Component {
             noEndDate: true,
             endDate: moment(),
             startDate: moment(),
-            ceType: '1',
+            ceType: 'unselected',
             meddra: React.createRef()
         };
         this._handleDateChange = this._handleDateChange.bind(this);
@@ -47,36 +47,35 @@ export class CreateCE extends Component {
         });
     }
 
-    componentDidMount() {
-        this.setState({
-            ceType: this.props.types[0].id
-        });
-    }
-
     _handleTypeChange(ev) {
         this.setState({
-            ceType: parseInt(ev.target.value, 10)
+            ceType: ev.target.value
         });
     }
 
     _formatRequestBody() {
         const date = this.state.startDate;
+        const filteredMeddra = this.props.meddra.filter(el => el.name === this.state.meddra.current.value)
+        const meddra = filteredMeddra && filteredMeddra[0] && !isNaN(parseInt(filteredMeddra[0].id)) ? parseInt(filteredMeddra[0].id) : undefined;
         return {
             patientId: this.props.match.params.patientId,
             data: {
                 patientId: this.props.patientId,
                 dateStartDate: date.toISOString(),
                 endDate: !this.state.noEndDate ? this.state.endDate.toISOString() : undefined,
-                type: Number.parseInt(this.state.ceType),
-                meddra: Number.parseInt(this.props.meddra.filter(el => el.name === this.state.meddra.current.value)[0].id)
+                type: this.state.ceType !== 'unselected' && !isNaN(parseInt(this.state.ceType)) ? parseInt(this.state.ceType) : undefined,
+                meddra: meddra
             }
         };
     }
 
     _handleSubmitClick() {
-        const meddra = this.props.meddra.filter(el => el.name === this.state.meddra.current.value);
-        if (meddra.length === 0) {
-            store.dispatch(addError({ error: 'The MedDRA code entered doesn\'t seem to be valid!' }));
+        console.log(this.state)
+
+        if (this.state.ceType === 'unselected') {
+            this.setState({
+                error: 'Please indicate the even type'
+            });
             return;
         }
         const requestBody = this._formatRequestBody();
@@ -98,11 +97,13 @@ export class CreateCE extends Component {
                         <label htmlFor='noEndDate'>The event is ongoing: </label><input type='checkbox' name='noEndDate' onChange={this._handleToggleEndDate} checked={this.state.noEndDate} /><br />
                         {this.state.noEndDate ? null : (<><label htmlFor='endDate'>End date: </label><PickDate startDate={this.state.endDate ? this.state.endDate : moment()} handleChange={this._handleEndDateChange} /><br /></>)}<br />
                         <label htmlFor='event'>What type of event is it?</label><br />
-                        <select name='event' value={this.state.testType} onChange={this._handleTypeChange} autoComplete='off'>
+                        <select name='event' value={this.state.ceType} onChange={this._handleTypeChange} autoComplete='off'>
+                            <option value='unselected'></option>
                             {this.props.types.map(type => <option key={type.id} value={type.id}>{type.name}</option>)}
                         </select> <br /><br />
                         <label htmlFor='meddra'>MedDRA:</label><br />
                         <SuggestionInput reference={this.state.meddra} /><br /><br />
+                        {this.state.error ? <><div className={style.error}>{this.state.error}</div><br /></> : null}
                         <button onClick={this._handleSubmitClick}>Submit</button>
                     </div>
                 </>
