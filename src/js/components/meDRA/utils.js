b5420fc3d524bcf5ff9cd8e1ef863bd0fe2eb622
Chon Sou <sou.chon@outlook.com>
2018-08-07 12:18:10 +0100

    done some clean up

--- src/js/components/meDRA/utils.js
+++ src/js/components/meDRA/utils.js
@@ -1,18 +1,23 @@
-export function formatTreeDataForAntd(entry) {    //entry = [ key, {text: 'whatever'}  ]
-    const key = entry[0];
-    const value = entry[1];
-    if (Object.keys(value).length === 1 && value.hasOwnProperty('text')){
-        return { value: key, key: key, title: value.text };
-    } else if (Object.keys(value).length !== 1 && value.hasOwnProperty('text')){
-        const text = value.text;
-        delete value.text;
-        return { value: key, key: key, title: text, children: Object.entries(value).map(formatTreeDataForAntd) };
-    } else {
-        throw Error(`${key} is wrong!`);
-    }
-}
+/* This function was used to generate tree array structure expected by antd tree-select with <TreeSelect treeData={treeData}>;
+    rendered useless when we opted for lazy loading but might need it in the future if we revert */
+// export function formatTreeDataForAntd(entry) {    //entry = [ key, {text: 'whatever'}  ]
+//     const key = entry[0];
+//     const value = entry[1];
+//     if (Object.keys(value).length === 1 && value.hasOwnProperty('text')){
+//         return { value: key, key: key, title: value.text };
+//     } else if (Object.keys(value).length !== 1 && value.hasOwnProperty('text')){
+//         const text = value.text;
+//         delete value.text;
+//         return { value: key, key: key, title: text, children: Object.entries(value).map(formatTreeDataForAntd) };
+//     } else {
+//         throw Error(`${key} is wrong!`);
+//     }
+// }
 
 
+
+
+/* not in used in react app but to generate seeds for data base */
 export function makeMeddraSeeds(tree) {
     let id = 1;
     const hash = [];
