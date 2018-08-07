5de2d5e9cd5b1848ca42acad85b05e3854116e14
Chon Sou <sou.chon@outlook.com>
2018-08-07 16:16:25 +0100

    allowing meddra to be optional in creating ce

--- src/js/components/patientProfile/patientChart.jsx
+++ src/js/components/patientProfile/patientChart.jsx
@@ -117,7 +117,7 @@ class ClinicalEvent extends PureComponent {
                 <td>{typedict[data.type]}</td>
                 <td>{date}</td>
                 <td>{endDate}</td>
-                <td>{meddraHash[data.meddra].name}</td>
+                <td>{data.meddra ? meddraHash[data.meddra].name : null}</td>
                 <td>
                     <NavLink id={`clinicalEvent/${data.id}`} to={`/patientProfile/${patientId}/data/clinicalEvent/${data.id}`} activeClassName={style.activeNavLink}>
                         <button>Data</button>
