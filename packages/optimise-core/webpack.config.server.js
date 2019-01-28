237c025f126a9c2449817a1eb6bb513ceb84c2da
Florian Guitton <f.guitton@imperial.ac.uk>
2019-01-28 10:18:23 +0000

    Adding webpack building for Express server

---
+++ packages/optimise-core/webpack.config.server.js
@@ -0,0 +1,39 @@
+const webpack = require('webpack');
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const StartServerPlugin = require('start-server-webpack-plugin');
+
+module.exports = {
+    mode: process.env.NODE_ENV || 'production',
+    entry: (process.env.NODE_ENV === 'development' ? ['webpack/hot/poll?1000'] : [])[
+        './src/index'
+    ],
+    watch: process.env.NODE_ENV === 'development' ? true : false,
+    target: 'node',
+    externals: [nodeExternals({
+        whitelist: process.env.NODE_ENV === 'development' ? ['webpack/hot/poll?1000'] : undefined
+    })],
+    module: {
+        rules: [{
+            test: /\.js?$/,
+            use: 'babel-loader',
+            exclude: /node_modules/
+        }]
+    },
+    plugins: (process.env.NODE_ENV === 'development' ? [
+        new StartServerPlugin('server.js'),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ] : []).concat([
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'BUILD_TARGET': JSON.stringify('server')
+            }
+        }),
+    ]),
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'server.js'
+    }
+};
