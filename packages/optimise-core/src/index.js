04a87ea392b8edd6759254414ce5a3aa1ebddc22
Florian Guitton <f.guitton@imperial.ac.uk>
2019-01-28 10:18:23 +0000

    Adding webpack building for Express server

--- packages/optimise-core/src/index.js
+++ packages/optimise-core/src/index.js
@@ -1,24 +1,51 @@
-let express = require('express');
+import http from 'http';
+import app from './server';
+
+const server = http.createServer(app);
+let currentApp = app;
+server.listen(3000);
+
+if (module.hot) {
+    module.hot.accept('./server', () => {
+        server.removeListener('request', currentApp);
+        server.on('request', app);
+        currentApp = app;
+    });
+}
+
+/*
+let http = require('http');
 let os = require('os');
 let config = require('../config/optimise.config');
 let OptimiseServer = require('./optimiseServer');
 
-let web_app = express();
 let optimise_server = new OptimiseServer(config);
+let server = null;
+let currentApp = null;
 
 optimise_server.start().then((optimise_router) => {
-    // Remove unwanted express headers
-    web_app.set('x-powered-by', false);
-    web_app.use(optimise_router);
-    web_app.listen(config.port, (error) => {
-        if (error) {
-            console.error(error); // eslint-disable-line no-console
-            return;
-        }
-        console.log(`Listening at http://${os.hostname()}:${config.port}/`); // eslint-disable-line no-console
-    });
+
+    server = http.createServer(optimise_router);
+    currentApp = optimise_router;
+    server.listen(3000);
+    console.log(`Listening at http://${os.hostname()}:${config.port}/`); // eslint-disable-line no-console
     return true;
 }).catch((error) => {
     console.error(error); // eslint-disable-line no-console
     return false;
 });
+
+if (module.hot) {
+    module.hot.accept('./optimiseServer', () => {
+        optimise_server.start().then((optimise_router) => {
+            server.removeListener('request', currentApp);
+            server.on('request', optimise_router);
+            currentApp = optimise_router;
+            return true;
+        }).catch((error) => {
+            console.error(error); // eslint-disable-line no-console
+            return false;
+        });
+    });
+}
+*/
