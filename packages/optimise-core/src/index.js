81d4a67126c325c5c7044144f2e490c1ee68d751
Florian Guitton <f.guitton@imperial.ac.uk>
2019-01-29 09:21:44 +0000

    Reverting core to legacy structure for now. Pending webpack conversion

--- packages/optimise-core/src/index.js
+++ packages/optimise-core/src/index.js
@@ -1,51 +1,24 @@
-import http from 'http';
-import app from './server';
-
-const server = http.createServer(app);
-let currentApp = app;
-server.listen(3030);
-
-if (module.hot) {
-    module.hot.accept('./server', () => {
-        server.removeListener('request', currentApp);
-        server.on('request', app);
-        currentApp = app;
-    });
-}
-
-/*
-let http = require('http');
+let express = require('express');
 let os = require('os');
 let config = require('../config/optimise.config');
 let OptimiseServer = require('./optimiseServer');
 
+let web_app = express();
 let optimise_server = new OptimiseServer(config);
-let server = null;
-let currentApp = null;
 
 optimise_server.start().then((optimise_router) => {
-
-    server = http.createServer(optimise_router);
-    currentApp = optimise_router;
-    server.listen(3000);
-    console.log(`Listening at http://${os.hostname()}:${config.port}/`); // eslint-disable-line no-console
+    // Remove unwanted express headers
+    web_app.set('x-powered-by', false);
+    web_app.use(optimise_router);
+    web_app.listen(config.port, (error) => {
+        if (error) {
+            console.error(error); // eslint-disable-line no-console
+            return;
+        }
+        console.log(`Listening at http://${os.hostname()}:${config.port}/`); // eslint-disable-line no-console
+    });
     return true;
 }).catch((error) => {
     console.error(error); // eslint-disable-line no-console
     return false;
 });
-
-if (module.hot) {
-    module.hot.accept('./optimiseServer', () => {
-        optimise_server.start().then((optimise_router) => {
-            server.removeListener('request', currentApp);
-            server.on('request', optimise_router);
-            currentApp = optimise_router;
-            return true;
-        }).catch((error) => {
-            console.error(error); // eslint-disable-line no-console
-            return false;
-        });
-    });
-}
-*/
