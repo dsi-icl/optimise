e11fb385c610b48d0bcd9a23b6308b9aa376833a
Florian Guitton <f.guitton@imperial.ac.uk>
2019-02-05 17:15:56 +0000

    Removing legacy portal

--- packages/optimise-portal/src/js/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { applyMiddleware, createStore } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import { rootReducer } from './reducers';
-
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
-window.store = store.getState;
-
-export default store;
