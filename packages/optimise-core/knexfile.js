b0923d6ae4a814a3362092bc5ae86eb40ca5c040
Florian Guitton <f.guitton@imperial.ac.uk>
2019-02-05 18:40:28 +0000

    Moving to a webpack running pipeline for optimise-core

--- packages/optimise-core/knexfile.js
+++ packages/optimise-core/knexfile.js
@@ -1,14 +1,13 @@
-const path = require('path');
 module.exports = {
     client: 'sqlite3',
     connection: {
-        filename: process.env.NODE_ENV === 'test' ? ':memory:' : path.normalize('./db/optimise-db.sqlite')
+        filename: process.env.NODE_ENV === 'test' ? ':memory:' : 'db/optimise-db.sqlite'
     },
     migrations: {
-        directory: path.normalize(`${path.dirname(__filename)}/db/migrations`)
+        directory: 'db/migrations'
     },
     seeds: {
-        directory: path.normalize(`${path.dirname(__filename)}/db/seed`)
+        directory: 'db/seed'
     },
     pool: {
         afterCreate: (conn, cb) => {
