c071dd906a810eb59e9294b59884c26f393ca7a2
pas-de-sou-c <sou.chon@outlook.com>
2019-02-07 15:31:14 +0000

    backend for meddra upload finished

--- packages/optimise-core/src/core/MeddraHierarchyProcessor.js
+++ packages/optimise-core/src/core/MeddraHierarchyProcessor.js
@@ -1,31 +1,41 @@
 const Papa = require('papaparse');
 
-function MeddraHierarchyProcessor(file) {
+function MeddraHierarchyProcessor(hierfile, lltfile) {
     this.parsebuffer = MeddraHierarchyProcessor.prototype.parsebuffer.bind(this);
     this.transformData = MeddraHierarchyProcessor.prototype.transformData.bind(this);
-    this.file = file;
-    this.data = [];
+    this.hierfile = hierfile;
+    this.lltfile = lltfile;
+    this.hierdata = [];
+    this.lltdata = [];
 }
 
 MeddraHierarchyProcessor.prototype.parsebuffer = function () {
-    const parser = Papa.parse(this.file.buffer.toString(), {
+    const hierparser = Papa.parse(this.hierfile.buffer.toString(), {
         delimiter: '$',
         header: false
     });
-    this.data = parser.data;
+    this.hierdata = hierparser.data;
+
+    if (this.lltfile) {
+        const lltparser = Papa.parse(this.lltfile.buffer.toString(), {
+            delimiter: '$',
+            header: false,
+            quoteChar: '\t',
+            escapeChar: '\t'
+        });
+        this.lltdata = lltparser.data;
+    }
 };
 
 MeddraHierarchyProcessor.prototype.transformData = function() {
-    // {
-    //     code: ###
-    //     name: ''
-    //     parent: ###
-    //     isLeaf
-    // }
     const names = {};
     const parents = {};
+    const leafs = [];
 
-    this.data.forEach(el => {
+    this.hierdata.forEach(el => {
+        if (!this.lltfile) {
+            leafs.push(el[0]);
+        }
         for (let i = 0; i < 4; i++) {
             if (el[i] !== undefined && el[i] !== '') {
                 names[el[i]] = el[i + 4];
@@ -35,23 +45,50 @@ MeddraHierarchyProcessor.prototype.transformData = function() {
                     } else {
                         parents[el[i]].add(el[i + 1]);
                     }
+                } else if (i === 3) {
+                    parents[el[i]] = null;
                 }
             }
+        }
+    });
+
+    this.lltdata.forEach(el => {
+        leafs.push(el[0]);
+        if (el.length !== 12) return;
+        names[el[0]] = el[1];
+        if (parents[el[0]] === undefined) {
+            parents[el[0]] = new Set([el[2]]);
+        } else {
+            parents[el[0]].add(el[2]);
         }
     });
 
     const transformedData = [];
     let id = 1;
     Object.keys(parents).forEach(key => {
-        let array = Array.from(parents[key]);
-        for (let i = 0; i < array.length; i++) {
+        if (parents[key] === null) {
             transformedData.push({
                 id,
                 code: key,
                 name: names[key] || 'Unknown name',
-                parentCode: array[i]
+                parentCode: null,
+                isLeaf: 0
             });
             id++;
+        } else {
+            let array = Array.from(parents[key]);
+            for (let i = 0; i < array.length; i++) {
+                if (key !== array[i]) {
+                    transformedData.push({
+                        id,
+                        code: key,
+                        name: names[key] || 'Unknown name',
+                        parentCode: array[i],
+                        isLeaf: leafs.includes(key) ? 1 : 0
+                    });
+                    id++;
+                }
+            }
         }
     });
 
@@ -62,11 +99,13 @@ MeddraHierarchyProcessor.prototype.transformData = function() {
         }
         idHash[el.code] = el.id;
     });
-
-    transformedData.forEach(el => {
-        
+    transformedData.map(el => {
+        el.parent = idHash[el.parentCode] || el.parentCode;
+        // delete el.parentCode;
+        return el;
     });
-    console.log(idHash);
+
+    return transformedData;
 };
 
 module.exports = MeddraHierarchyProcessor;
--- packages/optimise-core/src/routes/meddraRoute.js
+++ packages/optimise-core/src/routes/meddraRoute.js
@@ -8,6 +8,6 @@ const MeddraController = require('../controllers/meddraController');
 const medCtrl = new MeddraController();
 
 meddra.route('/')
-    .post(upload.single('file'), medCtrl.handleMeddraUploadByAdmin);
+    .post(upload.fields([{ name: 'lltfile', maxCount: 1 }, { name: 'mdhierfile', maxCount: 1 }]), medCtrl.handleMeddraUploadByAdmin);
 
 module.exports = meddra;
