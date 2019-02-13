43c5a8b436eaae0b1ff53d9280e06e70b630a807
pas-de-sou-c <sou.chon@outlook.com>
2019-02-13 12:53:57 +0000

    tests for meddra WIP

---
+++ packages/optimise-core/test/meddraUpload.test.js
@@ -0,0 +1,49 @@
+/* global beforeAll afterAll describe test expect */
+
+const request = require('supertest');
+const admin = request.agent(global.optimiseRouter);
+const FormData = require('form-data');
+const { connectAdmin, disconnectAgent } = require('./connection');
+
+beforeAll(async () => {
+    await connectAdmin(admin);
+});
+
+afterAll(async () => {
+    await disconnectAgent(admin);
+});
+
+describe('Meddra upload tests', () => {
+    test('Initially the meddra code database is empty', () => admin
+        .get('/meddra')
+        .then(res => {
+            expect(res.statusCode).toBe(200);
+            expect(res.body.length).toBe(0);
+            return true;
+        })
+    );
+
+
+    test('Uploading mdhier.asc', () => {
+        const form = new FormData();
+        form.append('mdhierfile', this.hierRef.current.files[0]);
+        return admin
+            .post('/uploadMeddra')
+            .send(form)
+            .then(res => {
+                expect(res.statusCode).toBe(200);
+                console.log(res);
+                return true;
+            });
+    });
+
+    test('meddra code database is filled', () => admin
+        .get('/meddra')
+        .then(res => {
+            expect(res.statusCode).toBe(200);
+            expect(res.body.length).toBe(0);
+            return true;
+        })
+    );
+
+});
