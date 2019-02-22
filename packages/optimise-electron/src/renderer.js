87753b9c672b0bed717cca7908481f331d277f45
Florian Guitton <f.guitton@imperial.ac.uk>
2019-02-22 16:51:23 +0000

    Changing the way the API fetcher is resolved

--- packages/optimise-electron/src/renderer.js
+++ packages/optimise-electron/src/renderer.js
@@ -1,36 +1,33 @@
 const { ipcRenderer } = require('electron');
 const callStack = {};
 
-document.addEventListener('DOMContentLoaded', function (event) {
+window['ipcUpdateCommander'] = () => {
+    ipcRenderer.send('quitAndInstall');
+};
 
-    window['ipcUpdateCommander'] = () => {
-        ipcRenderer.send('quitAndInstall');
-    };
+ipcRenderer.on('updateReady', function (event, text) {
+    window['ipcUpdateReady'] = text;
+});
 
-    ipcRenderer.on('updateReady', function (event, text) {
-        window['ipcUpdateReady'] = text;
+ipcRenderer.on('message', function (event, text) {
+    window['ipcUpdateStatus'] = text;
+});
+
+ipcRenderer.on('optimiseApiResult', function (event, { cid, res }) {
+    callStack[cid]({
+        json: () => Promise.resolve(res)
     });
+    delete callStack[cid];
+});
 
-    ipcRenderer.on('message', function (event, text) {
-        window['ipcUpdateStatus'] = text;
-    });
-
-    ipcRenderer.on('optimiseApiResult', function (event, { cid, res }) {
-        callStack[cid]({
-            json: () => Promise.resolve(res)
-        });
-        delete callStack[cid];
-    });
-
-    window['ipcFetch'] = (url, options) => {
-        return new Promise((resolve) => {
-            let cid = `${Math.random().toString(36).substr(2, 5)}`;
-            callStack[cid] = resolve;
-            ipcRenderer.send('optimiseApiCall', {
-                cid,
-                url,
-                options
-            })
+window['ipcFetch'] = (url, options) => {
+    return new Promise((resolve) => {
+        let cid = `${Math.random().toString(36).substr(2, 5)}`;
+        callStack[cid] = resolve;
+        ipcRenderer.send('optimiseApiCall', {
+            cid,
+            url,
+            options
         })
-    };
-})
+    })
+};
