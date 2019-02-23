14da68eef8adfe25ad8280b37928a5d703a89f77
Florian Guitton <f.guitton@imperial.ac.uk>
2019-02-23 14:48:36 +0000

    Adding server info route

---
+++ packages/optimise-core/src/controllers/infoController.js
@@ -0,0 +1,23 @@
+import ErrorHelper from '../utils/error_helper';
+import message from '../utils/message-utils';
+import InfoCore from '../core/info';
+import formatToJSON from '../utils/format-response';
+
+class InfoController {
+
+    static getInfo({ user }, res) {
+        if (user.priv !== 1) {
+            res.status(401).json(ErrorHelper(message.userError.NORIGHTS));
+            return;
+        }
+        InfoCore.getInfo().then((result) => {
+            res.status(200).json(formatToJSON(result));
+            return true;
+        }).catch((error) => {
+            res.status(400).json(ErrorHelper(message.errorMessages.GETFAIL, error));
+            return false;
+        });
+    }
+}
+
+export default InfoController;
