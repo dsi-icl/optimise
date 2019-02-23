924ddb0973b7bfc4a80b163e858ae62e123d05cc
Florian Guitton <f.guitton@imperial.ac.uk>
2019-02-23 10:07:11 +0000

    Fixing issue with the Helmet component triggering stack overflow. Replaced the react-helmet plugin for react-helmet-async.

--- packages/optimise-ui/src/components/admin/system.jsx
+++ packages/optimise-ui/src/components/admin/system.jsx
@@ -1,12 +1,13 @@
 import React, { PureComponent } from 'react';
-import packageInfo from '../../../package.json';
+// import corePackageInfo from '../../../../optimise-core/package.json';
+import uiPackageInfo from '../../../package.json';
 import style from './admin.module.css';
 
 export class SystemInfo extends PureComponent {
     render() {
         return (
             <>
-                This system relies on <pre className={style.packageName}>optimise-core</pre> v{packageInfo.dependencies['optimise-core']}
+                This system relies on <pre className={style.packageName}>optimise-core</pre> v and <pre className={style.packageName}>optimise-ui</pre> v{uiPackageInfo.version}
             </>
         );
     }
--- packages/optimise-ui/src/index.js
+++ packages/optimise-ui/src/index.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { HelmetProvider } from 'react-helmet-async';
 import { Router } from 'react-router-dom';
 import App from './application';
 import store from './redux/store';
@@ -12,7 +13,9 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
     <Provider store={store}>
         <Router history={history}>
-            <App />
+            <HelmetProvider>
+                <App />
+            </HelmetProvider>
         </Router>
     </Provider>,
     document.getElementById('root'));
