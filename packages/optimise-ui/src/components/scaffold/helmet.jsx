52565f0947a43730611a7eb56831fe09486f67c2
sou-chon <sou.chon@outlook.com>
2019-04-27 20:40:12 +0100

    Reverting HELMET import??????

--- packages/optimise-ui/src/components/scaffold/helmet.jsx
+++ packages/optimise-ui/src/components/scaffold/helmet.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Helmet } from 'react-helmet-async';
+import Helmet from 'react-helmet-async';
 import appInfo from '../../../package.json';
 
 export default class LocalHelmet extends Component {
