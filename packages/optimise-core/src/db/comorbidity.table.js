39f1c4e45afae5d0f5a09f3f4105bc7a0c073a71
sou-chon <sou.chon@outlook.com>
2019-05-03 14:24:48 +0100

    correcting db version in migration for comorbidity

--- packages/optimise-core/src/db/comorbidity.table.js
+++ packages/optimise-core/src/db/comorbidity.table.js
@@ -2,7 +2,7 @@ export const TABLE_NAME = 'COMORBIDITY';
 export const PRIORITY = 3;
 export default async (dbcon, version) => {
     switch (version) {
-        case 1:
+        case 2:
             if (await dbcon().schema.hasTable(TABLE_NAME) === true)
                 await dbcon().schema.renameTable(TABLE_NAME, `ARCHIVE_${Date.now()}_${TABLE_NAME}`);
             await dbcon().schema.createTable(TABLE_NAME, (table) => {
