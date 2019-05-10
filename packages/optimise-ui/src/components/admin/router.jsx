a8819b7ec662e4be8980da731d7233c312139c6c
Florian Guitton <f.guitton@imperial.ac.uk>
2019-05-10 17:17:19 +0100

    This is implementing the admin panel for patient ID mapping export

--- packages/optimise-ui/src/components/admin/router.jsx
+++ packages/optimise-ui/src/components/admin/router.jsx
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Log } from './log';
 import { Users } from './users';
-import { Messages } from './messages';
 import { SystemInfo } from './system';
 import { Meddra } from './meddra';
 import { Update } from './update';
+import { PatientMappings } from './patientMappings';
 
 export class AdminRouter extends Component {
     render() {
@@ -13,10 +13,10 @@ export class AdminRouter extends Component {
             <Switch>
                 <Route exact path='/administration/log' render={({ match }) => <Log match={match} />} />
                 <Route path='/administration/users' render={({ match }) => <Users match={match} />} />
-                <Route exact path='/administration/messages' render={({ match }) => <Messages match={match} />} />
                 <Route exact path='/administration/system' render={({ match }) => <SystemInfo match={match} />} />
                 <Route exact path='/administration/meddra' render={({ match }) => <Meddra match={match} />} />
                 <Route exact path='/administration/update' render={({ match }) => <Update match={match} />} />
+                <Route exact path='/administration/patientMappings' render={({ match }) => <PatientMappings match={match} />} />
                 <Route path='/' component={() => <></>} />
             </Switch>
         );
