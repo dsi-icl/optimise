d18b36933a6dc58044057ba06defdeb6ecf35b89
Florian Guitton <f.guitton@imperial.ac.uk>
2019-07-09 19:30:39 +0100

    [UI] Add sync status query

--- packages/optimise-ui/src/components/scaffold/syncIndicator.jsx
+++ packages/optimise-ui/src/components/scaffold/syncIndicator.jsx
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { getSyncStatusAPICall } from '../../redux/actions/syncInfo';
 import Icon from '../icon';
 import style from './scaffold.module.css';
 
 @connect(state => ({
     syncInfo: state.syncInfo
+}), dispatch => ({
+    getSyncStatus: () => dispatch(getSyncStatusAPICall())
 }))
 export default class StatusBar extends Component {
+
+    constructor() {
+        super();
+        this._updateStatus = this._updateStatus.bind(this);
+    }
+
+    componentDidMount() {
+        this.statusUpdater = setInterval(this._updateStatus, 10000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.statusUpdater);
+    }
+
+    _updateStatus() {
+        this.props.getSyncStatus();
+    }
+
     render() {
         const { syncInfo } = this.props;
         if (syncInfo.config === undefined || syncInfo.config.host === undefined || syncInfo.config.host === '')
