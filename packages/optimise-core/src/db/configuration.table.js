737f7ab43a98ada5a0042d556775672c7dd02bc7
Florian Guitton <f.guitton@imperial.ac.uk>
2019-07-09 14:43:18 +0100

    Correcting DB upgrade process to include empty valued keys for sync host and key

--- packages/optimise-core/src/db/configuration.table.js
+++ packages/optimise-core/src/db/configuration.table.js
@@ -5,9 +5,21 @@ export const PRIORITY = 0;
 export default async (dbcon, version) => {
     switch (version) {
         case 3:
-            return dbcon()('OPT_KV').insert({
+            await dbcon()('OPT_KV').insert({
                 key: 'SYNC_AGENT_ID',
                 value: uuid(),
+                created_at: dbcon().fn.now(),
+                updated_at: dbcon().fn.now()
+            });
+            await dbcon()('OPT_KV').insert({
+                key: 'SYNC_HOST',
+                value: '',
+                created_at: dbcon().fn.now(),
+                updated_at: dbcon().fn.now()
+            });
+            return dbcon()('OPT_KV').insert({
+                key: 'SYNC_KEY',
+                value: '',
                 created_at: dbcon().fn.now(),
                 updated_at: dbcon().fn.now()
             });
