8d7c3cddbb7a07854e40f7b28073a1c468f709c8
Florian Guitton <f.guitton@imperial.ac.uk>
2019-07-10 17:22:03 +0100

    Enabling dumb synchronisation of patient profiles and users.

--- packages/optimise-core/src/core/sync.js
+++ packages/optimise-core/src/core/sync.js
@@ -1,5 +1,8 @@
+import os from 'os';
+import request from 'request';
 import { getEntry } from '../utils/controller-utils';
 import dbcon from '../utils/db-connection';
+import PatientCore from './patient';
 import ErrorHelper from '../utils/error_helper';
 import message from '../utils/message-utils';
 
@@ -78,7 +81,9 @@ class SyncCore {
         return new Promise((resolve, reject) => {
             if (config === undefined)
                 return reject(ErrorHelper(message.errorMessages.UPDATEFAIL, 'Sync configuration not initialized or invalid'));
-            setTimeout(() => SyncCore.startSync(config), 1000);
+            setTimeout(() => {
+                SyncCore.startSync(config).catch(err => console.error('plop 5', err));
+            }, 1000);
             return resolve({
                 status: 'success'
             });
@@ -86,15 +91,86 @@ class SyncCore {
     }
 
     /**
- * @function startSync start synchronization.
- *
+     * @function startSync start synchronization.
+     *
      * @param {*} config Connection information for synchronization
- */
-    static startSync(config) {
-        console.log('Syncing ...');
-        return {
-            status: 'success'
+     */
+    static async startSync(config) {
+        const patients = await dbcon().select().table('PATIENTS');
+        const users = await dbcon().select().table('USERS');
+        if (patients.length <= 0)
+            return;
+        let patientPromises = [];
+        let patientProfiles = [];
+        patients.forEach(patient => {
+            patientPromises.push(PatientCore.getPatientProfile({ 'id': patient.id }, true).then(result => {
+                patientProfiles.push({
+                    ...patient,
+                    ...result,
+                    aliasId: undefined,
+                    patientId: undefined
+                });
+                return true;
+            }).catch(err => console.error('plop 3', err)));
+        });
+        await Promise.all(patientPromises).catch(err => console.error('plop 4', err));
+        const data = JSON.stringify({
+            uuid: config.id,
+            oshost: os.hostname(),
+            key: config.key,
+            data: {
+                patients: patientProfiles,
+                users: users.map(user => {
+                    delete user.pw;
+                    delete user.salt;
+                    delete user.iterations;
+                    return user;
+                })
+            }
+        });
+
+        const options = {
+            uri: `${config.host}api/sync/v1`,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': data.length
+            },
+            body: data
         };
+        try {
+            request(options, (error, response, body) => {
+                if (!error && response.statusCode === 200) {
+                    const result = JSON.parse(body);
+                    console.log('plop 1', result);
+                }
+            });
+        } catch (e) {
+            console.error('plop 2', e);
+        }
+
+        return Promise.resolve();
+        // const tables = [
+        //     'ADVERSE_EVENT_MEDDRA',
+        //     'CLINICAL_EVENTS',
+        //     'CLINICAL_EVENTS_DATA',
+        //     'LOG_ACTIONS',
+        //     'MEDICAL_HISTORY',
+        //     'ORDERED_TESTS',
+        //     'PATIENTS',
+        //     'PATIENT_DEMOGRAPHICS',
+        //     'PATIENT_DIAGNOSIS',
+        //     'PATIENT_IMMUNISATION',
+        //     'PATIENT_PII',
+        //     'PATIENT_PREGNANCY',
+        //     'TEST_DATA',
+        //     'TREATMENTS',
+        //     'TREATMENTS_INTERRUPTIONS',
+        //     'USERS',
+        //     'VISITS',
+        //     'VISIT_DATA',
+        //     'VISIT_REPORT'
+        // ];
     }
 }
 
