c0fd505b9e607519f10adef6c4c32ea574d6938d
Florian Guitton <f.guitton@imperial.ac.uk>
2019-07-11 16:42:00 +0100

    Adding sync status DB field

--- packages/optimise-core/src/db/configuration.table.js
+++ packages/optimise-core/src/db/configuration.table.js
@@ -5,24 +5,27 @@ export const PRIORITY = 0;
 export default async (dbcon, version) => {
     switch (version) {
         case 3:
-            await dbcon()('OPT_KV').insert({
+            return dbcon()(TABLE_NAME).insert([{
                 key: 'SYNC_AGENT_ID',
                 value: uuid(),
                 created_at: dbcon().fn.now(),
                 updated_at: dbcon().fn.now()
-            });
-            await dbcon()('OPT_KV').insert({
+            }, {
                 key: 'SYNC_HOST',
                 value: '',
                 created_at: dbcon().fn.now(),
                 updated_at: dbcon().fn.now()
-            });
-            return dbcon()('OPT_KV').insert({
+            }, {
                 key: 'SYNC_KEY',
                 value: '',
                 created_at: dbcon().fn.now(),
                 updated_at: dbcon().fn.now()
-            });
+            }, {
+                key: 'SYNC_STATUS',
+                value: '{}',
+                created_at: dbcon().fn.now(),
+                updated_at: dbcon().fn.now()
+            }]);
         default:
             break;
     }
