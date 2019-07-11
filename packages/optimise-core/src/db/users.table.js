25ae694deff6eab985ad706d07a511fd1ddc2131
Florian Guitton <f.guitton@imperial.ac.uk>
2019-07-11 14:34:27 +0100

    Updating the USER table to create UUID and email for sync purposes

--- packages/optimise-core/src/db/users.table.js
+++ packages/optimise-core/src/db/users.table.js
@@ -1,3 +1,4 @@
+import uuid from 'uuid/v4';
 import { generateAndHash } from '../utils/generate-crypto';
 let hashedAdmin;
 
@@ -25,6 +26,33 @@ export default async (dbcon, version) => {
             return dbcon()(TABLE_NAME).insert([
                 { id: 1, username: 'admin', realName: 'Administrator', pw: hashedAdmin.hashed, salt: hashedAdmin.salt, iterations: hashedAdmin.iteration, adminPriv: 1, createdByUser: 1 },
             ]);
+        case 3:
+            const OLD_TABLE_NAME = `ARCHIVE_V2_${Date.now()}_${TABLE_NAME}`;
+            if (await dbcon().schema.hasTable(TABLE_NAME) === true)
+                await dbcon().schema.renameTable(TABLE_NAME, OLD_TABLE_NAME);
+            await dbcon().schema.createTable(TABLE_NAME, (table) => {
+                table.increments('id').primary();
+                table.text('uuid').notNullable();
+                table.text('username').notNullable();
+                table.text('realname').notNullable();
+                table.text('email').notNullable();
+                table.text('pw').notNullable();
+                table.text('salt').notNullable();
+                table.integer('iterations').notNullable();
+                table.integer('adminPriv').notNullable();
+                table.timestamp('createdTime').notNullable().defaultTo(dbcon().fn.now());
+                table.integer('createdByUser').notNullable().references('id').inTable('USERS');
+                table.text('deleted').notNullable().defaultTo('-');
+                table.unique(['username', 'deleted'], `UNIQUE_${Date.now()}_${TABLE_NAME}`);
+            });
+
+            const oldData = await dbcon()(OLD_TABLE_NAME).select('*');
+            for (let i = 0; i < oldData.length; i++) {
+                oldData[i].uuid = uuid();
+                oldData[i].email = `${uuid()}@ic.ac.uk`;
+            }
+            await dbcon().batchInsert(TABLE_NAME, oldData, 50);
+            break;
         default:
             break;
     }
