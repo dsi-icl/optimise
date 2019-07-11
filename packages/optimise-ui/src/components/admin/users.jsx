1b849111071a8ecebc0e4a59a1ff2a0d9602db2f
Florian Guitton <f.guitton@imperial.ac.uk>
2019-07-11 16:40:33 +0100

    [UI] Revamping the user management interface

--- packages/optimise-ui/src/components/admin/users.jsx
+++ packages/optimise-ui/src/components/admin/users.jsx
@@ -2,22 +2,9 @@ import React, { Component, PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import store from '../../redux/store';
-import { createUserAPICall, getAllUsersAPICall } from '../../redux/actions/admin';
+import { getAllUsersAPICall } from '../../redux/actions/admin';
 import Icon from '../icon';
 import style from './admin.module.css';
-
-
-export class Users extends Component {
-    render() {
-        return (
-            <>
-                <UserList />
-                <CreateUser />
-            </>
-        );
-    }
-}
-
 
 @connect(state => ({
     getAllUsers: state.getAllUsers
@@ -36,7 +23,13 @@ export class UserList extends Component {
                 return <div> Cannot fetch.. </div>;
             } else {
                 const users = result.slice(Math.max(0, result.length - 100));
-                return <>{users.map(el => <UserEntry key={el.id} entry={el} />)}</>;
+                return (
+                    <>
+                        {
+                            users.map(el => <UserEntry key={el.id} entry={el} />)
+                        }
+                    </>
+                );
             }
         }
     }
@@ -60,75 +53,3 @@ export class UserEntry extends PureComponent {    /* consider mapping the endpoi
     }
 }
 
-
-class CreateUser extends Component {
-    constructor() {
-        super();
-        this.state = { addMore: false, error: false };
-        this.usernameRef = React.createRef();
-        this.realnameRef = React.createRef();
-        this.isAdminRef = React.createRef();
-        this.pwRef = React.createRef();
-        this._handleClickingAdd = this._handleClickingAdd.bind(this);
-        this._handleSubmit = this._handleSubmit.bind(this);
-    }
-
-    _handleClickingAdd() {
-        this.setState({ addMore: !this.state.addMore });
-    }
-
-    _handleSubmit() {
-        for (let each of [this.usernameRef, this.realnameRef, this.pwRef]) {
-            if (each.current.value === '') {
-                this.setState({ error: true });
-                return;
-            }
-        }
-        const body = {
-            username: this.usernameRef.current.value,
-            pw: this.pwRef.current.value,
-            realname: this.realnameRef.current.value,
-            isAdmin: this.isAdminRef.current.checked ? 1 : 0
-        };
-        store.dispatch(createUserAPICall(body));
-        this.setState({ addMore: false, error: false });
-    }
-
-    render() {
-        return (
-            <>
-                {!this.state.addMore ?
-                    <button onClick={this._handleClickingAdd}>Create new user</button>
-                    :
-                    <>
-                        <table>
-                            <thead>
-                                <tr><th>Username</th><th>Real name</th><th>Password</th><th>Is admin?</th></tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>
-                                        <input type='text' ref={this.usernameRef} />
-                                    </td>
-                                    <td>
-                                        <input type='text' ref={this.realnameRef} />
-                                    </td>
-                                    <td>
-                                        <input type='text' ref={this.pwRef} />
-                                    </td>
-                                    <td>
-                                        <input type='checkbox' ref={this.isAdminRef} />
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table><br />
-                        <br />
-                        {this.state.error ? <><div className={style.error}>None of the fields can be empty!</div><br /><br /></> : null}
-                        <button onClick={this._handleSubmit}>Submit</button><br /><br />
-                        <button onClick={this._handleClickingAdd}>Cancel</button>
-                    </>
-                }
-            </>
-        );
-    }
-}
