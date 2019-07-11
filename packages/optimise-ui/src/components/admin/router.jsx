05b056363f5d292cd7ab75d250269cfdf8b49a58
Florian Guitton <f.guitton@imperial.ac.uk>
2019-07-11 16:40:33 +0100

    [UI] Revamping the user management interface

--- packages/optimise-ui/src/components/admin/router.jsx
+++ packages/optimise-ui/src/components/admin/router.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Log } from './log';
-import { Users } from './users';
+import { UserList } from './users';
 import { SystemInfo } from './system';
 import { Meddra } from './meddra';
 import { Update } from './update';
@@ -13,7 +13,7 @@ export class AdminRouter extends Component {
         return (
             <Switch>
                 <Route exact path='/administration/log' render={({ match }) => <Log match={match} />} />
-                <Route path='/administration/users' render={({ match }) => <Users match={match} />} />
+                <Route path='/administration/users' render={({ match }) => <UserList match={match} />} />
                 <Route exact path='/administration/system' render={({ match }) => <SystemInfo match={match} />} />
                 <Route exact path='/administration/meddra' render={({ match }) => <Meddra match={match} />} />
                 <Route exact path='/administration/update' render={({ match }) => <Update match={match} />} />
---
+++ packages/optimise-ui/src/components/admin/userCreatePage.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import store from '../../redux/store';
+import { BackButton } from '../medicalData/utils';
+import { createUserAPICall } from '../../redux/actions/admin';
+import style from './admin.module.css';
+
+const email_reg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+export class UserCreate extends Component {
+    constructor() {
+        super();
+        this.state = { error: false };
+        this.usernameRef = React.createRef();
+        this.realnameRef = React.createRef();
+        this.isAdminRef = React.createRef();
+        this.emailRef = React.createRef();
+        this.pwRef = React.createRef();
+        this.pwConRef = React.createRef();
+        this._handleSubmit = this._handleSubmit.bind(this);
+    }
+
+    _handleSubmit() {
+        for (let each of [this.usernameRef, this.realnameRef, this.pwRef, this.pwConRef, this.emailRef]) {
+            if (each.current.value === '') {
+                this.setState({ error: true });
+                return;
+            }
+        }
+        if (this.pwRef.current.value !== this.pwConRef.current.value) {
+            this.setState({ error: true });
+            return;
+        }
+        if (!email_reg.test(this.emailRef.current.value)) {
+            this.setState({ error: true });
+            return;
+        }
+        const body = {
+            username: this.usernameRef.current.value,
+            pw: this.pwRef.current.value,
+            email: this.emailRef.current.value,
+            realname: this.realnameRef.current.value,
+            isAdmin: this.isAdminRef.current.checked ? 1 : 0
+        };
+        store.dispatch(createUserAPICall(body));
+        this.setState({ error: false });
+    }
+
+    render() {
+        return (
+            <>
+                <div className={style.ariane}>
+                    <h2>+ NEW USER</h2>
+                    <BackButton to={'/administration/users'} />
+                </div>
+                <div className={style.userDetailPanel}>
+                    <div className={style.userDetail}>
+                        <label htmlFor='username'>Username:</label><br /><input name='username' type='text' ref={this.usernameRef} /><br /><br />
+                        <label htmlFor='realname'>Real name:</label><br /><input name='realname' type='text' ref={this.realnameRef} /><br /><br />
+                        <label htmlFor='email'>Email:</label><br /><input name='email' type='text' ref={this.emailRef} /><br /><br />
+                        <label htmlFor='password'>Password:</label><br /><input name='password' type='password' ref={this.pwRef} /><br /><br />
+                        <label htmlFor='passwordConf'>Confirm Password:</label><br /><input name='passwordConf' type='password' ref={this.pwConRef} /><br /><br />
+                        <label htmlFor='isadmin'>Is Admin:</label><input name='isadmin' type='checkbox' ref={this.isAdminRef} /><br /><br />
+                        {this.state.error ? <><div className={style.error}>None of the fields can be empty!</div><br /><br /></> : null}
+                        <button onClick={this._handleSubmit}>Create</button><br /><br />
+                    </div>
+                </div>
+            </>
+        );
+    }
+}
