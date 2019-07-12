38f22b0c0eb968e7c9c20c1486a1579e3c878ed8
Florian Guitton <f.guitton@imperial.ac.uk>
2019-07-12 16:00:18 +0100

    [UI] Fixing uncontrolled component behaviour in synchronisation configuration panel

--- packages/optimise-ui/src/components/admin/sync.jsx
+++ packages/optimise-ui/src/components/admin/sync.jsx
@@ -12,11 +12,17 @@ import style from './admin.module.css';
 }))
 export class Sync extends PureComponent {
 
-    constructor() {
-        super();
-        this.state = { error: false };
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: false,
+            id: props.syncInfo.config.id,
+            host: props.syncInfo.config.host,
+            key: props.syncInfo.config.key
+        };
         this.syncAddress = React.createRef();
         this.syncKey = React.createRef();
+        this._handleInputChange = this._handleInputChange.bind(this);
         this._handleSubmit = this._handleSubmit.bind(this);
         this._handleSync = this._handleSync.bind(this);
     }
@@ -25,9 +31,31 @@ export class Sync extends PureComponent {
         this.props.getSyncOptions();
     }
 
+    static getDerivedStateFromProps(nextProps, prevState) {
+        if (nextProps.syncInfo.config.id === prevState.id)
+            return prevState;
+        return {
+            ...prevState,
+            id: nextProps.syncInfo.config.id,
+            host: nextProps.syncInfo.config.host,
+            key: nextProps.syncInfo.config.key
+        };
+    }
+
     _handleSubmit() {
-        if (this.syncAddress.current.value === '' || this.syncKey.current.value === '') {
-            this.setState({ error: 'Please enter remote syncing information' });
+        try {
+            if (this.syncAddress.current.value !== '')
+                new URL(this.syncAddress.current.value);
+        } catch (e) {
+            this.setState({ error: 'Invalid Remote Host' });
+            return;
+        }
+        if (this.syncAddress.current.value === '' && this.syncKey.current.value !== '') {
+            this.setState({ error: 'Missing Remote Host information' });
+            return;
+        }
+        if (this.syncAddress.current.value !== '' && this.syncKey.current.value === '') {
+            this.setState({ error: 'Missing a Validation Key' });
             return;
         }
         const body = {
@@ -42,15 +70,22 @@ export class Sync extends PureComponent {
         this.props.syncNow();
     }
 
+    _handleInputChange(event) {
+        this.setState({ [`${event.target.attributes.name}`]: event.target.value });
+    }
+
     render() {
-        const { syncInfo: { config: { id, host, key } } } = this.props;
+        const { id, host, key } = this.state;
+
+        if (id === undefined)
+            return null;
 
         return (
             <>
                 <p>Optimise can connect to a central site to synchronise the content of your database.</p><br />
-                <label htmlFor='id'>Agent ID:</label><br /> <input name='id' type='text' readOnly value={id} autocomplete='off' autocorrect='off' autocapitalize='off' spellcheck='false' /><br /><br />
-                <label htmlFor='host'>Remote Host:</label><br /> <input name='host' type='text' ref={this.syncAddress} defaultValue={host} autocomplete='off' autocorrect='off' autocapitalize='off' spellcheck='false' /><br /><br />
-                <label htmlFor='key'>Validation Key:</label><br /> <input name='key' type='text' ref={this.syncKey} defaultValue={key} autocomplete='off' autocorrect='off' autocapitalize='off' spellcheck='false' /><br /><br />
+                <label htmlFor='id'>Agent ID:</label><br /> <input name='id' type='text' readOnly value={id} autoComplete='off' autoCorrect='off' autoCapitalize='off' spellCheck='false' /><br /><br />
+                <label htmlFor='host'>Remote Host:</label><br /> <input name='host' type='text' ref={this.syncAddress} defaultValue={host} autoComplete='off' autoCorrect='off' autoCapitalize='off' spellCheck='false' /><br /><br />
+                <label htmlFor='key'>Validation Key:</label><br /> <input name='key' type='text' ref={this.syncKey} defaultValue={key} autoComplete='off' autoCorrect='off' autoCapitalize='off' spellCheck='false' /><br /><br />
                 {this.state.error ? <><div className={style.error}>{this.state.error}</div><br /></> : null}
                 <button onClick={this._handleSubmit} >Save Connection Information</button><br /><br />
                 <button onClick={this._handleSync} >Synchronize now</button>
