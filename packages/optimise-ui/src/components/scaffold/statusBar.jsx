1faf01651801d525483d35d63b20fdb7855242da
Florian Guitton <f.guitton@imperial.ac.uk>
2019-07-12 20:48:26 +0100

    [UI] Fixing a flag where the status bar would show up too early

--- packages/optimise-ui/src/components/scaffold/statusBar.jsx
+++ packages/optimise-ui/src/components/scaffold/statusBar.jsx
@@ -6,15 +6,16 @@ import packageInfo from '../../../package.json';
 
 @connect(state => ({
     username: state.login.username,
+    fetching: state.availableFields.fetching
 }))
 export default class StatusBar extends Component {
     render() {
         let version = packageInfo.version;
         if (window && window.optimiseVersion)
             version = window.optimiseVersion;
-        const { username } = this.props;
+        const { username, fetching } = this.props;
         return (
-            <div className={style.statusBar} style={{ visibility: username !== '' ? 'visible' : 'hidden' }}>
+            <div className={style.statusBar} style={{ visibility: (username !== '' && fetching !== true) ? 'visible' : 'hidden' }}>
                 <span> Logged in as {username}</span>
                 <SyncIndicator></SyncIndicator>
                 <span className={style.rightPush}> OptimiseMS v{version}</span>
