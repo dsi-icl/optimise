98f21e341889d3c86556a9bf67af94278c621d5c
Florian Guitton <f.guitton@imperial.ac.uk>
2019-07-21 20:28:25 +0100

    Fixing a regression where the patient corresponding to deleted records would be attempted multiple times for migration.

--- packages/optimise-core/src/db/patient_demographic.table.js
+++ packages/optimise-core/src/db/patient_demographic.table.js
@@ -16,34 +16,38 @@ export default async (dbcon, version) => {
                 // We verify if we are already at version 2
                 if (oldData.length > 0 && oldData[0].smokingHistory !== undefined) {
 
-                    const existingPatientsInThisTable = await dbcon()(OLD_TABLE_NAME).select('*');
+                    const eligiblePatients = await dbcon()(OLD_TABLE_NAME).select('*').where({ deleted: '-' });
                     const smokingHash = (await dbcon()('SMOKING_HISTORY').select('*')).reduce((a, e) => { a[e.id] = e.value; return a; }, {});
                     const alcoholHash = (await dbcon()('ALCOHOL_USAGE').select('*')).reduce((a, e) => { a[e.id] = e.value; return a; }, {});
 
-                    for (let each of existingPatientsInThisTable) {
+                    for (let each of eligiblePatients) {
 
                         /* copying data to visit data */
                         const patientID = each['patient'];
                         const alcoholUsage = each['alcoholUsage'];
                         const smokingHistory = each['smokingHistory'];
                         const createdByUser = each['createdByUser'];
-                        const maxVisitId = (await dbcon()('VISITS').min('id').where({ patient: patientID, deleted: '-', type: 1 }))[0]['min(`id`)'];
+                        const eligibleVisits = (await dbcon()('VISITS').min('id').where({ patient: patientID, deleted: '-', type: 1 }));
 
-                        if (maxVisitId !== null) {
-                            await dbcon()('VISIT_DATA').insert([
-                                {
-                                    visit: maxVisitId,
-                                    field: 252,
-                                    value: smokingHash[smokingHistory],
-                                    createdByUser
-                                },
-                                {
-                                    visit: maxVisitId,
-                                    field: 253,
-                                    value: alcoholHash[alcoholUsage],
-                                    createdByUser
-                                },
-                            ]);
+                        if (eligibleVisits.length > 0) {
+
+                            const maxVisitId = eligibleVisits[0]['min(`id`)'];
+                            if (maxVisitId !== null && maxVisitId !== undefined) {
+                                await dbcon()('VISIT_DATA').insert([
+                                    {
+                                        visit: maxVisitId,
+                                        field: 252,
+                                        value: smokingHash[smokingHistory],
+                                        createdByUser
+                                    },
+                                    {
+                                        visit: maxVisitId,
+                                        field: 253,
+                                        value: alcoholHash[alcoholUsage],
+                                        createdByUser
+                                    },
+                                ]);
+                            }
                         }
                     }
 
