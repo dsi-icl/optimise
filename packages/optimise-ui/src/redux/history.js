a74937afaf4d53251ee52c2b5ad96f89e179e1e4
Florian Guitton <f.guitton@imperial.ac.uk>
2019-08-02 17:24:25 +0100

    Fixing issue when resources could not be fetched in electron due to the react-router changing the URL pathname

--- packages/optimise-ui/src/redux/history.js
+++ packages/optimise-ui/src/redux/history.js
@@ -1,3 +1,7 @@
 import { createBrowserHistory } from 'history';
 
-export default createBrowserHistory();
+export default createBrowserHistory({
+    // Because we use Router in react-route-dom instead of MemoryRouter we need to set the base of the URL to the resource folder on the hard drive.
+    // This allows images and other resources to load relative to their actual location
+    basename: window.ipcFetch !== undefined ? window.location.pathname.slice(0, 0 - 'index.html'.length) : undefined
+});
