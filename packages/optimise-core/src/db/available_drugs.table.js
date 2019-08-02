9c835d3c2b7f9a726d2b7669851df4a63a14b4fd
Florian Guitton <f.guitton@imperial.ac.uk>
2019-08-02 15:04:14 +0100

    Removing previous disease modifying drugs from the list and adding the new version

--- packages/optimise-core/src/db/available_drugs.table.js
+++ packages/optimise-core/src/db/available_drugs.table.js
@@ -1,4 +1,5 @@
-import drugs from './defaults_v1/drugs.json';
+import drugs_v1 from './defaults_v1/drugs.json';
+import drugs_v4 from './defaults_v4/drugs.json';
 
 export const TABLE_NAME = 'AVAILABLE_DRUGS';
 export const PRIORITY = 0;
@@ -12,7 +13,13 @@ export default async (dbcon, version) => {
                 table.text('deleted').notNullable().defaultTo('-');
                 table.unique(['name', 'module', 'deleted'], `UNIQUE_${Date.now()}_${TABLE_NAME}`);
             });
-            await dbcon()(TABLE_NAME).insert(drugs);
+            await dbcon()(TABLE_NAME).insert(drugs_v1);
+            break;
+        case 4:
+            await dbcon()(TABLE_NAME)
+                .where('module', 'Disease Modifying')
+                .update({ 'deleted': `0@${(new Date()).getTime()}` });
+            await dbcon()(TABLE_NAME).insert(drugs_v4);
             break;
         default:
             break;
