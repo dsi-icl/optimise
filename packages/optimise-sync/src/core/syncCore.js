0ab21916c5858ba23bbb28b7c0ac92d4bc68a5e3
Florian Guitton <f.guitton@imperial.ac.uk>
2019-09-02 16:51:37 +0100

    Adding sync attempt record and key validation for clients.

--- packages/optimise-sync/src/core/syncCore.js
+++ packages/optimise-sync/src/core/syncCore.js
@@ -2,10 +2,10 @@ import dbcon from '../utils/db-connection';
 class SyncCore {
 
     /**
-     * @function updatePatient updates a patient profile
+     * @function updatePatient updates patient profiles
      *
      * @param {*} agent Identifier for the agent sending the data
-     * @param {*} profile Patient Profile to save
+     * @param {*} profiles Patient Profile to save
      * @returns a Promise that contains the result from the select query
      */
     static async updatePatientProfiles(agent, profiles) {
@@ -20,10 +20,10 @@ class SyncCore {
     }
 
     /**
-     * @function updatePatient updates a patient profile
+     * @function updateUsers updates users
      *
      * @param {*} agent Identifier for the agent sending the data
-     * @param {*} profile Patient Profile to save
+     * @param {*} users Users to save
      * @returns a Promise that contains the result from the select query
      */
     static async updateUsers(agent, users) {
@@ -35,6 +35,55 @@ class SyncCore {
             }, { upsert: true }).then(({ result }) => result));
         });
         return Promise.all(inserts);
+    }
+
+    /**
+     * @function createSyncRecord record the occurrence of a synchronisation
+     *
+     * @param {*} agent Identifier for the agent sending the data
+     * @param {*} info Extra information about the sync event
+     * @returns a Promise that contains the result from the select query
+     */
+    static async createSyncRecord(agent, info) {
+        const db = await dbcon().then(client => client.db());
+        return db.collection(`EVENTS`).insertOne({
+            uuid: agent,
+            version: info.version,
+            hostname: info.hostname,
+            ip: info.ip,
+            time: (new Date()).toISOString(),
+            error: info.error
+        });
+    }
+
+    /**
+     * @function validateKey verifies a key is valid
+     *
+     * @param {*} agent Identifier for the agent sending the data
+     * @param {*} key Validation key offered by the remote client
+     * @returns a Promise that contains the result from the select query
+     */
+    static async validateKey(agent, key) {
+        const db = await dbcon().then(client => client.db());
+        const record = await db.collection('VALIDATION_KEYS').findOne({ key });
+        let error = undefined;
+        if (record === null || record === undefined)
+            error = 'The validation key does not exist'
+        else {
+            if (record.claim === undefined) {
+                await db.collection('VALIDATION_KEYS').updateOne({ _id: record._id }, {
+                    $set: {
+                        claim: agent
+                    }
+                }, { upsert: false })
+            } else if (record.claim !== agent) {
+                error = 'The validation key is claimed by a different agent'
+            }
+        }
+        return Promise.resolve({
+            success: error === undefined ? true : false,
+            error
+        });
     }
 }
 
