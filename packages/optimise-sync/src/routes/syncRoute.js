47de690ce4cc1c75839aebfb9fb971a9052db2b4
Florian Guitton <f.guitton@imperial.ac.uk>
2019-10-22 13:58:50 +0100

    Branching out version 1.0 and 1.1 of sync API

--- packages/optimise-sync/src/routes/syncRoute.js
+++ packages/optimise-sync/src/routes/syncRoute.js
@@ -10,7 +10,15 @@ const ce = express();
 import CeController from '../controllers/syncController';
 
 ce.route('/v1')
-    .post(CeController.createSync)
+    .post(CeController.createSyncV1_0)
+    .get(CeController.checkSync);
+
+ce.route('/v1.0')
+    .post(CeController.createSyncV1_0)
+    .get(CeController.checkSync);
+
+ce.route('/v1.1')
+    .post(CeController.createSyncV1_1)
     .get(CeController.checkSync);
 
 export default ce;
