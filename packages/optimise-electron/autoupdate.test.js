fcf543fb9c0b080b8bbb8423fbccab3e7f5d2f03
Florian Guitton <f.guitton@imperial.ac.uk>
2019-12-13 11:55:35 +0000

    Only testing autoupdate for Windows for now.

--- packages/optimise-electron/autoupdate.test.js
+++ packages/optimise-electron/autoupdate.test.js
@@ -1,58 +1,70 @@
-const fs = require('fs-extra');
-const package = JSON.parse(fs.readFileSync('package.json'));
+// Only Windows binaries are currrently published on GitHub.
+if (process.platform !== "win32") {
+    console.log('Bypassing auto-update tests for non Windows hosts.');
+    process.exit(0);
+}
 
-package.version = "0.0.1";
+try {
+    const fs = require('fs-extra');
+    const package = JSON.parse(fs.readFileSync('package.json'));
 
-fs.writeFile('package.json', JSON.stringify(package), (err) => {
+    package.version = "0.0.1";
 
-    if (err) {
-        console.error('\nFile package.json could not overwritten');
-        process.exit(1);
-    }
+    fs.writeFile('package.json', JSON.stringify(package, null, 4), (err) => {
 
-    const spawn = require('child_process').spawn;
-    const bin = require('electron');
-    const args = ['.', '--autoupdate-test'];
-    const cspr = spawn(bin, args);
-
-    let timeoutCounter = null;
-
-    cspr.stdout.setEncoding('utf8');
-    cspr.stdout.on('data', function (data) {
-        const str = data.toString();
-        const lines = str.split(/(\r?\n)/g);
-        for (var i = 0; i < lines.length; i++) {
-            process.stdout.write(lines[i]);
-            if (/\(100/.test(lines[i])) {
-                console.log('\nSetting up readiness timeout...');
-                timeoutCounter = setTimeout(() => {
-                    console.error('\nUpdate has failed !');
-                    cspr.kill("SIGTERM");
-                    process.exit(1);
-                }, 30000);
-            }
-            if (/The update is ready/.test(lines[i])) {
-                console.log('\nSuccessfuly update !');
-                clearTimeout(timeoutCounter);
-                cspr.kill("SIGTERM");
-                process.exit(0);
-            }
+        if (err) {
+            console.error('\nFile package.json could not overwritten');
+            process.exit(1);
         }
-    });
 
-    cspr.stderr.on('data', function (data) {
-        data += '';
-        process.stderr.write(lines[i]);
-    });
+        const spawn = require('child_process').spawn;
+        const bin = require('electron');
+        const args = ['.', '--autoupdate-test'];
+        const cspr = spawn(bin, args);
 
-    cspr.on('exit', function (code) {
-        console.log('child process exited with code ' + code);
-        process.exit(code);
-    });
+        let timeoutCounter = null;
 
-    cspr.on('error', function (error) {
-        console.error('child process had an error ' + error);
-        process.exit(1);
-    });
+        cspr.stdout.setEncoding('utf8');
+        cspr.stdout.on('data', function (data) {
+            const str = data.toString();
+            const lines = str.split(/(\r?\n)/g);
+            if (lines)
+                for (var i = 0; i < lines.length; i++) {
+                    process.stdout.write(lines[i]);
+                    if (/\(100/.test(lines[i])) {
+                        console.log('\nSetting up readiness timeout...');
+                        timeoutCounter = setTimeout(() => {
+                            console.error('\nUpdate has failed !');
+                            cspr.kill("SIGTERM");
+                            process.exit(1);
+                        }, 30000);
+                    }
+                    if (/The update is ready/.test(lines[i])) {
+                        console.log('\nSuccessfuly update !');
+                        clearTimeout(timeoutCounter);
+                        cspr.kill("SIGTERM");
+                        process.exit(0);
+                    }
+                }
+        });
 
-});
+        cspr.stderr.on('data', function (data) {
+            data += '';
+            process.stderr.write(lines[i]);
+        });
+
+        cspr.on('exit', function (code) {
+            console.log('child process exited with code ' + code);
+            process.exit(code);
+        });
+
+        cspr.on('error', function (error) {
+            console.error('child process had an error ' + error);
+            process.exit(1);
+        });
+
+    });
+} catch (e) {
+    console.error('Update test failed', e);
+    process.exit(1);
+}
