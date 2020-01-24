a1aaca7d6e34e4d0f771c5ddc06291b6f6ddca74
Florian Guitton <f.guitton@imperial.ac.uk>
2020-01-24 14:33:23 +0000

    Adding migration from documentation to this branche

--- packages/optimise-ui/src/components/userManual/userManual.jsx
+++ packages/optimise-ui/src/components/userManual/userManual.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactMarkdown from 'react-markdown';
-import ImageZoom from 'react-medium-image-zoom';
+import Zoom from 'react-medium-image-zoom';
 import Helmet from '../scaffold/helmet';
 import style from './userManual.module.css';
 import markup from './userManual.md';
@@ -20,15 +20,9 @@ export default class UserManual extends Component {
                 </div>
                 <div className={style.panel}>
                     <ReactMarkdown source={markup} escapeHtml={false} renderers={{
-                        image: ({ alt, src }) => <ImageZoom
-                            image={{
-                                src,
-                                alt,
-                            }}
-                            zoomImage={{
-                                className: style.bordered
-                            }}
-                        />
+                        image: ({ alt, src }) => <Zoom>
+                            <img alt={alt} src={src} className={style.bordered} />
+                        </Zoom>
                     }} />
                 </div>
             </>
