578c8f729d9e0b6541e3a9f5e9ecb2ca6d3fe9b8
Florian Guitton <f.guitton@imperial.ac.uk>
2020-03-25 18:49:15 +0000

    Move to consistent naming for countries addition

--- packages/optimise-core/src/db/countries.table.js
+++ packages/optimise-core/src/db/countries.table.js
@@ -1,5 +1,5 @@
 import countries from './defaults_v1/countries.json';
-import v7_countries from './defaults_v7/new_countries.json';
+import v7_countries from './defaults_v7/countries.json';
 
 export const TABLE_NAME = 'COUNTRIES';
 export const PRIORITY = 0;
