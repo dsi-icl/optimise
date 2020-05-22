60a5f030924ba493ff2a086aeafe2f40fd7141db
sou-chon <sou.chon@outlook.com>
2020-05-22 21:23:14 +0100

    adding pregnancies to frontpage

--- packages/optimise-ui/src/components/createVisitFrontPage/componentWrappers/vs/vsWrapper.jsx
+++ packages/optimise-ui/src/components/createVisitFrontPage/componentWrappers/vs/vsWrapper.jsx
@@ -3,15 +3,31 @@ import { VisitData } from '../../../medicalData/visitDataPage';
 import override_style from '../overrideStyle.module.css';
 import style from '../../frontpage.module.css';
 import scaffold_style from '../scaffoldStyle.module.css';
+import { connect } from 'react-redux';
 import { FrontPageNavigationButton } from '../navigationButtons/navigationButtons';
+import EditPregnancy from '../../../editMedicalElements/editPregnancy';
 
+@connect(state => ({
+    data: state.patientProfile.data,
+}))
 export class VSFrontPageWrapper extends Component {
     render() {
         return (
             <>
                 <div className={style.page}>
                     <div className={scaffold_style.padding_div}>
-                        <p>Please enter vital signs:</p>
+                        {
+                            this.props.data.demographicData && this.props.data.demographicData.gender !== 1
+                                ?
+                                <>
+                                    <p style={{ marginBottom: 0, fontSize: '1.1rem' }}>Please update this list of pregnancies:</p>
+                                    <EditPregnancy match={this.props.match} location={this.props.location} renderedInFrontPage={true}/>
+                                    <br/><br/>
+                                </>
+                                :
+                                null
+                        }
+                        <p style={{ fontSize: '1.1rem' }}>Please enter vital signs:</p>
                         <VisitData childRef={component => { this.form = component; }} elementType='visit' match={this.props.match} category={'vitals'} override_style={override_style} renderedInFrontPage={true} />
                     </div>
                 </div>
