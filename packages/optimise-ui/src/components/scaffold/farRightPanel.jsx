d68d84fdbb5456633b778096d541272c28dcdedf
sou-chon <sou.chon@outlook.com>
2021-03-01 14:56:58 +0000

    example pregnancy forms

--- packages/optimise-ui/src/components/scaffold/farRightPanel.jsx
+++ packages/optimise-ui/src/components/scaffold/farRightPanel.jsx
@@ -13,28 +13,34 @@ import { UserDetail } from '../admin/userDetailPage';
 import { UserActions } from '../admin/userActions';
 import { UserManualMenu } from '../userManual';
 import style from './scaffold.module.css';
+import { EditPregnancies } from '../pregnancyForms/editPregnancy';
+
 
 export default class FarRightPanel extends Component {
     render() {
         return (
             <div className={style.farRightPanel}>
-                <Switch>
-                    <Route path='/patientProfile/:patientId/create/:type' render={({ match }) => <CreateElementRouter match={match} />} />
-                    <Route path='/patientProfile/:patientId/edit/:elementType/:elementId' render={({ match }) => <EditElementRouter match={match} />} />
-                    <Route path='/patientProfile/:patientId/data/:elementType/:elementId' render={({ match }) => <DataPageRouter match={match} />} />
-                    <Route path='/patientProfile/:patientId/createVisit' render={({ match }) => <CreateVisit match={match} />} />
-                    <Route path='/patientProfile/:patientId' render={({ match }) => <Section match={match} />} />
-                    <Route exact path='/searchPatient/from/deletionSuccessful' component={() => <DeletedPatient />} />
-                    <Route exact path='/createPatient/:patientIdCreated' render={({ match }) => <CreatePatient match={match} />} />
-                    <Route exact path='/createPatient/' render={() => <CreatePatient />} />
-                    <Route exact path='/filterPatients' component={() => <></>} />
-                    <Route exact path='/administration/users/create' render={({ match }) => <UserCreate match={match} />} />
-                    <Route exact path='/administration/users/:userId' render={({ match }) => <UserDetail match={match} />} />
-                    <Route exact path='/administration/users' render={({ match }) => <UserActions match={match} />} />
-                    <Route path='/administration' component={() => <></>} />
-                    <Route path='/userManual' component={UserManualMenu} />
-                    <Route path='/' render={({ match }) => <ExportSets match={match} />} />
-                </Switch>
+            <Switch>
+            {
+                /*<Route path='/' render={({ match }) => <PregnancyBaselineDataForm/>} /> */
+            }
+            <Route path='/patientProfile/:patientId/create/:type' render={({ match }) => <CreateElementRouter match={match} />} />
+            <Route path='/patientProfile/:patientId/edit/:elementType/:elementId' render={({ match }) => <EditElementRouter match={match} />} />
+            <Route path='/patientProfile/:patientId/data/:elementType/:elementId' render={({ match }) => <DataPageRouter match={match} />} />
+            <Route path='/patientProfile/:patientId/createVisit' render={({ match }) => <CreateVisit match={match} />} />
+            <Route path='/patientProfile/:patientId/editPregnancyDataEntry/:entryId' render={({ match }) => <EditPregnancies match={match} />} />
+            <Route path='/patientProfile/:patientId' render={({ match }) => <Section match={match} />} />
+            <Route exact path='/searchPatient/from/deletionSuccessful' component={() => <DeletedPatient />} />
+            <Route exact path='/createPatient/:patientIdCreated' render={({ match }) => <CreatePatient match={match} />} />
+            <Route exact path='/createPatient/' render={() => <CreatePatient />} />
+            <Route exact path='/filterPatients' component={() => <></>} />
+            <Route exact path='/administration/users/create' render={({ match }) => <UserCreate match={match} />} />
+            <Route exact path='/administration/users/:userId' render={({ match }) => <UserDetail match={match} />} />
+            <Route exact path='/administration/users' render={({ match }) => <UserActions match={match} />} />
+            <Route path='/administration' component={() => <></>} />
+            <Route path='/userManual' component={UserManualMenu} />
+            <Route path='/' render={({ match }) => <ExportSets match={match} />} />
+            </Switch>
             </div>
         );
     }
