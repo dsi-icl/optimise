829553b86287299688169df5b321ad40bbe9c576
sou-chon <sou.chon@outlook.com>
2021-03-01 14:57:13 +0000

    pregnancy tables db

---
+++ packages/optimise-core/src/db/patient_pregnancy_imaging.js
@@ -0,0 +1,21 @@
+export const TABLE_NAME = 'PATIENT_PREGNANCY_IMAGING';
+export const PRIORITY = 4;
+export default async (dbcon, version) => {
+    switch (version) {
+        case 12:
+            await dbcon().schema.createTable(TABLE_NAME, (table) => {
+                table.increments('id').primary();
+                table.integer('pregnancyDataId').notNullable().references('id').inTable('PATIENT_PREGNANCY_DATA').onDelete('CASCADE');
+                table.text('date').notNullable();
+                table.text('deleted').notNullable();
+                table.text('mode').notNullable();
+                table.text('result').notNullable();
+
+                table.unique(['pregnancyDateId', 'date', 'deleted'], `UNIQUE_${Date.now()}_${TABLE_NAME}`);
+            });
+            await tableCopyBack(TABLE_NAME);
+            break;
+        default:
+            break;
+    }
+};
