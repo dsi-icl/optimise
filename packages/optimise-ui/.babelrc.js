2b65a0c410219c69bedbb851441115c41e03be14
Florian Guitton <f.guitton@imperial.ac.uk>
2024-07-28 20:29:51 +0100

    chore: Cleaning up package dependencies

--- packages/optimise-ui/.babelrc.js
+++ packages/optimise-ui/.babelrc.js
@@ -1,4 +1,18 @@
 module.exports = {
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                loose: true
+                // targets: {
+                //     esmodules: true
+                // }
+            }
+        ],
+        [
+            '@babel/preset-react'
+        ]
+    ],
     plugins: [
         '@babel/plugin-syntax-jsx',
         ['@babel/plugin-proposal-decorators', {
