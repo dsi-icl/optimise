9595260793a3c5c9b54704b0bf230684616b3a2f
Florian Guitton <f.guitton@imperial.ac.uk>
2025-02-11 15:04:15 +0000

    fix(docker): Assets would not be served correctly

--- packages/optimise-docker/launcher.js
+++ packages/optimise-docker/launcher.js
@@ -1,26 +1,33 @@
 const express = require('express');
-const rateLimit = require('express-rate-limit');
+const { default: rateLimit } = require('express-rate-limit');
 const compression = require('compression');
 const OptimiseServer = require('./server').default;
 const path = require('path');
+const fs = require('fs');
 
 const root = express();
 const optimise = new OptimiseServer();
 
 optimise.start().then(router => {
 
+    console.log(fs.readdirSync(`${__dirname}/static`));
+
     // For production activating reponse compression
     root.use(compression());
+
+    // Plug rate limitation
+    root.use(rateLimit({
+        windowMs: 1 * 60 * 1000,
+        max: 500
+    }));
 
     // Linking optimise's router on /api
     root.use('/api', router);
 
     // Binding static resources folder
-    root.use('/static', express.static(path.normalize(`${__dirname}/static`)));
-
-    root.use(rateLimit({
-        windowMs: 1 * 60 * 1000,
-        max: 500
+    root.use('/', express.static(path.normalize(`${__dirname}/static`), {
+        redirect: false,
+        fallthrough: true
     }));
 
     // Referencing any other requests to the /public/index.html
