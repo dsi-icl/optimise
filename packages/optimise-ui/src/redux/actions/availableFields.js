58a2f5514bff266686979886071e8b3508e6a03c
Florian Guitton <f.guitton@imperial.ac.uk>
2025-02-19 20:48:50 +0000

    fix(ui): Revert previous change that bypass meddra condings fetch

--- packages/optimise-ui/src/redux/actions/availableFields.js
+++ packages/optimise-ui/src/redux/actions/availableFields.js
@@ -24,8 +24,7 @@ export const getMeddraSuccess = (payload) => ({ type: actions.availableFields.GE
 export const getMeddraCall = APICall('/meddra', getMeddraSuccess);
 
 export const getICD11Success = (payload) => ({ type: actions.availableFields.GET_ICD11_SUCCESS, payload: payload });
-// export const getICD11Call = APICall('/icd11', getICD11Success);
-export const getICD11Call = () => (dispatch) => dispatch(getICD11Success([]));
+export const getICD11Call = APICall('/icd11', getICD11Success);
 
 export const getPregnancyOutcomesSuccess = (payload) => ({ type: actions.availableFields.GET_PREGNANCY_OUTCOMES_SUCCESS, payload: payload });
 export const getPregnancyOutcomesCall = APICall('/demographics/Fields/Pregnancy', getPregnancyOutcomesSuccess);
